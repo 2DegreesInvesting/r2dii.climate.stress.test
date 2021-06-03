d22dcc04c9fa03826af98d4b4a102eb25402292a
jacobvjk <jacob.kastl@gmail.com>
2021-06-03 12:04:02 +0200

    set temporary envvar in test

--- tests/testthat/test-get_st_data_path.R
+++ tests/testthat/test-get_st_data_path.R
@@ -1,5 +1,8 @@
 test_that("default returns an fs path", {
-  testthat::expect_s3_class(get_st_data_path(), "fs_path")
+  withr::with_envvar(
+    new = c("ST_DATA_PATH" = "path/to/data"),
+    testthat::expect_s3_class(get_st_data_path(), "fs_path")
+  )
 })
 
 test_that("non existing envvar and undefined options return error", {
