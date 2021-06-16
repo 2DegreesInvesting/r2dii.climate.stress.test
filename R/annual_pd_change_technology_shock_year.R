730275b3caefb1b741f01e3a423567b49d1490ff
jacobvjk <jacob.kastl@gmail.com>
2021-06-16 09:39:31 +0200

    add graphing functions for annual credit risk and dependencies

---
+++ R/annual_pd_change_technology_shock_year.R
@@ -0,0 +1,67 @@
+#' Display the annual pd changes per shock year and technology.
+#'
+#' @param data A data.frame that contains Merton output data.
+#' @param shock_year_filter Numeric. Vector that specifies the respective start
+#'   years of different policy shocks.
+#' @param geography_filter Character. A vector containing the geographical
+#'   specification of the scenario
+#'
+#' @family qa graphing functions
+#'
+#' @return ggplot object
+#'
+#' @export
+annual_pd_change_technology_shock_year <- function(data,
+                                                   shock_year_filter = NULL,
+                                                   geography_filter = NULL) {
+  force(data)
+
+  shock_year_filter %||% stop("Must provide input for 'shock_year_filter'", call. = FALSE)
+  geography_filter %||% stop("Must provide input for 'geography_filter'", call. = FALSE)
+
+  data_has_expected_columns <- all(c(
+    "scenario_name", "ald_sector", "technology",
+    "scenario_geography", "year", "PD_change_late_sudden"
+  )
+  %in% colnames(data))
+  stopifnot(data_has_expected_columns)
+
+  shock_year_filter <- stringr::str_c("Carbon balance ", shock_year_filter)
+
+  plot <- data %>%
+    dplyr::filter(
+      .data$scenario_name %in% shock_year_filter &
+        .data$scenario_geography %in% geography_filter
+    ) %>%
+    dplyr::mutate(
+      scenario_name = stringr::str_replace(
+        .data$scenario_name,
+        "Carbon balance",
+        "Shock year"
+      )
+    ) %>%
+    ggplot(
+      aes(
+        # TODO: must display full years
+        x = .data$year,
+        y = .data$PD_change_late_sudden * 100,
+        fill = .data$PD_change_late_sudden * 100
+      )
+    ) +
+    geom_col(position = "dodge") +
+    scale_fill_gradient2(low = "blue", mid = "lightgrey", high = "red") +
+    labs(
+      x = "Year",
+      y = "PD change in % points",
+      title = "Annual PD change by shock year, technology"
+    ) +
+    facet_grid(
+      rows = vars(.data$ald_sector, .data$technology),
+      cols = vars(.data$scenario_name),
+      scales = "free"
+    ) +
+    r2dii.plot::theme_2dii() +
+    theme(
+      axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+    )
+}
--- tests/testthat/test-calculate_annual_pd_changes.R
+++ tests/testthat/test-calculate_annual_pd_changes.R
@@ -1,1 +1,6 @@
-#
+test_that("without specified arguments, function throws error", {
+  testthat::expect_error(
+    calculate_pd_change_annual(),
+    "argument \"data\" is missing"
+  )
+})
