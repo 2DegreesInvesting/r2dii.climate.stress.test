ce1fc2110ab40f241c8616015c1b82a99ad5e1b6
MirijaHa <mirja@2degrees-investing.org>
2021-08-13 13:51:16 +0200

    func removal 15

--- 0_portfolio_test.R
+++ 0_portfolio_test.R
@@ -60,28 +60,6 @@ calculate_with_weights <- function(df, weight_col_name, weight_method_name) {
   df
 }
 
-aggregate_map_data <- function(portfolio){
-
-  portfolio <- portfolio %>%
-    ungroup() %>%
-    group_by(!!!rlang::syms(grouping_variables), allocation,
-             ald_location, year,
-             ald_sector, technology,
-             financial_sector, allocation, allocation_weight, ald_production_unit) %>%
-    summarise(plan_alloc_wt_tech_prod = sum(plan_alloc_wt_tech_prod, na.rm=TRUE),
-              .groups = "drop_last") %>%
-    mutate(plan_alloc_wt_sec_prod=sum(plan_alloc_wt_tech_prod))
-
-  if (data_check(portfolio)){
-
-    portfolio$equity_market <- "Global"
-    portfolio$scenario <- NA # this is current plans only
-    portfolio$scenario_geography <- NA
-  }
-
-  return(portfolio)
-}
-
 calculate_weights <- function(portfolio, portfolio_type, grouping_variables){
 
   port_sub <- portfolio %>%
