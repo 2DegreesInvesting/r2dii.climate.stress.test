805c23970e1a37db95447b83bf4c6787a198ffc4
jacobvjk <60064070+jacobvjk@users.noreply.github.com>
2021-08-16 11:02:14 +0200

    1184 read ngfs carbon tax (#28)
    * add function to read ngss carbon tax data
    
    * update documentation
    
    * update workflows to read ngfs carbon tax data

---
+++ R/read_ngfs_carbon_tax.R
@@ -0,0 +1,39 @@
+#' Read in NGFS carbon tax data from csv and check that all expected columns
+#' are given.
+#'
+#' @param path A string that points to the location of the file containing the
+#'   NGFS carbon tax information
+#'
+#' @family import functions
+#'
+#' @export
+read_ngfs_carbon_tax <- function(path = NULL) {
+  path %||% stop("Must provide 'path'")
+
+  valid_input_file_path <- file.exists(file.path(path))
+  stopifnot(valid_input_file_path)
+
+  data <- readr::read_csv(
+    path,
+    col_types = readr::cols(
+      year = "d",
+      model = "c",
+      scenario = "c",
+      scenario_geography = "c",
+      variable = "c",
+      unit = "c",
+      carbon_tax = "d"
+    )
+  )
+
+  expected_columns <- c(
+    "year", "model", "scenario", "scenario_geography", "variable", "unit",
+    "carbon_tax"
+  )
+
+  data_has_expected_columns <- all(expected_columns %in% colnames(data))
+  stopifnot(data_has_expected_columns)
+
+  return(data)
+
+}
