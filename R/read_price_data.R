550beedd3299e2ddbe5ac32ecaa3a5834f69da20
jacobvjk <jacob.kastl@gmail.com>
2021-08-24 10:13:44 +0200

    add function to read price data safely

---
+++ R/read_price_data.R
@@ -0,0 +1,39 @@
+#' Read in price data from csv and check that all expected columns are given.
+#'
+#' @param path A string that points to the location of the file containing the
+#'   price data
+#'
+#' @family import functions
+#'
+#' @export
+read_price_data <- function(path = NULL) {
+  path %||% stop("Must provide 'path'")
+
+  valid_input_file_path <- file.exists(file.path(path))
+  stopifnot(valid_input_file_path)
+
+  data <- readr::read_csv(
+    path,
+    col_types = readr::cols(
+      year = "d",
+      source = "c",
+      scenario = "c",
+      scenario_geography = "c",
+      technology = "c",
+      indicator = "c",
+      unit = "c",
+      price = "d"
+    )
+  )
+
+  expected_columns <- c(
+    "year", "source", "scenario", "scenario_geography", "technology",
+    "indicator", "unit", "price"
+  )
+
+  data_has_expected_columns <- all(expected_columns %in% colnames(data))
+  stopifnot(data_has_expected_columns)
+
+  return(data)
+# TODO: add check that looks if all technologies are given (requires new input arg)
+}
