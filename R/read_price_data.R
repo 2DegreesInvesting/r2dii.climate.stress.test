e27028210aab02d99f5ed667bd6dfc66d968121c
jacobvjk <jacob.kastl@gmail.com>
2021-08-24 12:02:02 +0200

    update read_price data to choose between versions

--- R/read_price_data.R
+++ R/read_price_data.R
@@ -2,38 +2,29 @@
 #'
 #' @param path A string that points to the location of the file containing the
 #'   price data
+#' @param version A string indicating whether to prepare the capacity factors
+#'   based on the old price data structure or the new long format. Must be
+#'   either "old" or "new".
 #'
 #' @family import functions
 #'
 #' @export
-read_price_data <- function(path = NULL) {
+read_price_data <- function(path = NULL,
+                            version = NULL) {
   path %||% stop("Must provide 'path'")
+  version %||% stop("Must provide 'version'")
 
   valid_input_file_path <- file.exists(file.path(path))
   stopifnot(valid_input_file_path)
 
-  data <- readr::read_csv(
-    path,
-    col_types = readr::cols(
-      year = "d",
-      source = "c",
-      scenario = "c",
-      scenario_geography = "c",
-      technology = "c",
-      indicator = "c",
-      unit = "c",
-      price = "d"
-    )
-  )
+  version_allowed <- version %in% c("old", "new")
+  stopifnot(version_allowed)
 
-  expected_columns <- c(
-    "year", "source", "scenario", "scenario_geography", "technology",
-    "indicator", "unit", "price"
-  )
-
-  data_has_expected_columns <- all(expected_columns %in% colnames(data))
-  stopifnot(data_has_expected_columns)
+  if (version == "new") {
+    data <- read_price_data_internal(path = path)
+  } else {
+    data <- read_price_data_internal_old(path = path)
+  }
 
   return(data)
-# TODO: add check that looks if all technologies are given (requires new input arg)
 }
--- tests/testthat/test-read_price_data.R
+++ tests/testthat/test-read_price_data.R
@@ -1,22 +1,92 @@
 test_that("without specified arguments, read_price_data() throws error", {
-            testthat::expect_error(
-              read_price_data(),
-              "Must provide 'path'"
-            )
-          })
+  testthat::expect_error(
+    read_price_data(),
+    "Must provide 'path'"
+  )
+})
 
-test_that("with valid arguments set, read_price_data() return data.frame", {
+test_that("with missing argument for version, read_price_data() throws error", {
   test_data_prices <- tibble::tribble(
     ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
     2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
     2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
   )
 
-  test_data_prices %>% readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
+  test_data_prices %>%
+    readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
 
   test_input_path <- file.path(tempdir(), "prices_data_input.csv")
 
-  test_data <- read_price_data(path = test_input_path)
-
-  testthat::expect_s3_class(test_data, "data.frame")
+  testthat::expect_error(
+    read_price_data(path = test_input_path),
+    "Must provide 'version'"
+  )
 })
+
+test_that("with valid arguments set, read_price_data() returns data.frame with
+          both old and new versions", {
+  test_data_prices_new <- tibble::tribble(
+    ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
+    2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
+    2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
+  )
+
+  test_data_prices_new %>%
+    readr::write_csv(file.path(tempdir(), "new_prices_data_input.csv"))
+
+  test_input_path_new <- file.path(tempdir(), "new_prices_data_input.csv")
+
+  test_version <- "new"
+
+  test_data_new <- read_price_data(
+    path = test_input_path_new,
+    version = test_version
+  )
+
+  testthat::expect_s3_class(test_data_new, "data.frame")
+
+  test_data_prices_old <- tibble::tribble(
+    ~year, ~sector, ~technology, ~sector_unit_ds, ~price_unit_iea, ~price_unit_etr, ~B2DS, ~b2ds_source, ~NPS, ~nps_source, ~SDS, ~sds_source, ~Baseline, ~baseline_source,
+    2020, "Power", "HydroCap", "MW", "Dollars per MwH", "USD per MWh", NA_real_, "test_source", 10, "test_source", 10, "test_source", 10, "custom",
+    2030, "Coal", "Coal", "TPA", "US$2010/GJ", "Dollars per ton", 10, "test_source", 10, "test_source", 10, "test_source", 10, "custom"
+  )
+
+  test_data_prices_old %>%
+    readr::write_csv(file.path(tempdir(), "old_prices_data_input.csv"))
+
+  test_input_path_old <- file.path(tempdir(), "old_prices_data_input.csv")
+
+  test_version <- "old"
+
+  test_data_old <- read_price_data(
+    path = test_input_path_old,
+    version = test_version
+  )
+
+  testthat::expect_s3_class(test_data_old, "data.frame")
+})
+
+test_that("with invalid argument for version, read_price_data() returns throws
+          a helpful error", {
+  test_data_prices_new <- tibble::tribble(
+    ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
+    2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
+    2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
+  )
+
+  test_data_prices_new %>%
+    readr::write_csv(file.path(tempdir(), "new_prices_data_input.csv"))
+
+  test_input_path_new <- file.path(tempdir(), "new_prices_data_input.csv")
+
+  test_version <- "invalid"
+
+  testthat::expect_error(
+    read_price_data(
+      path = test_input_path_new,
+      version = test_version
+    ),
+    "version_allowed is not TRUE"
+  )
+})
+
