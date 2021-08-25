7a5bd71ff00ba8542e3a6f5041baa1e02eb317db
MirijaHa <mirja@2degrees-investing.org>
2021-08-25 10:22:23 +0200

    temp test

--- R/read_price_data_internal_old.R
+++ R/read_price_data_internal_old.R
@@ -1,6 +1,7 @@
 #' Read in price data from csv and check that all expected columns are given.
 #'
-#' @description This function reads in price data using the old wide data format.
+#' @description This function reads in price data using long file format.
+#'   It is expected to work with data based on IEA WEO 2020.
 #' @inheritParams read_price_data
 #'
 #' @export
@@ -14,30 +15,24 @@ read_price_data_internal_old <- function(path = NULL) {
     path,
     col_types = readr::cols(
       year = "d",
-      sector = "c",
+      source = "c",
+      scenario = "c",
+      scenario_geography = "c",
       technology = "c",
-      sector_unit_ds = "c",
-      price_unit_iea = "c",
-      price_unit_etr = "c",
-      B2DS = "d",
-      b2ds_source = "c",
-      NPS = "d",
-      nps_source = "c",
-      SDS = "d",
-      sds_source = "c",
-      Baseline = "d",
-      baseline_source = "c"
+      indicator = "c",
+      unit = "c",
+      price = "d"
     )
   )
 
   expected_columns <- c(
-    "year", "sector", "technology", "sector_unit_ds", "price_unit_iea",
-    "price_unit_etr", "B2DS", "b2ds_source", "NPS", "nps_source", "SDS",
-    "sds_source", "Baseline", "baseline_source"
+    "year", "source", "scenario", "scenario_geography", "technology",
+    "indicator", "unit", "price"
   )
 
   data_has_expected_columns <- all(expected_columns %in% colnames(data))
   stopifnot(data_has_expected_columns)
 
   return(data)
+  # TODO: add check that looks if all technologies are given (requires new input arg)
 }
--- tests/testthat/test-read_price_data_internal_old.R
+++ tests/testthat/test-read_price_data_internal_old.R
@@ -1,27 +1,27 @@
 test_that("without specified arguments, read_price_data_internal_old() throws
           error", {
-  testthat::expect_error(
-    read_price_data_internal_old(),
-    "Must provide 'path'"
-  )
-})
+            testthat::expect_error(
+              read_price_data_internal_old(),
+              "Must provide 'path'"
+            )
+          })
 
 test_that("with valid arguments set, read_price_data_internal_old() returns
           data.frame", {
-  test_data_prices <- tibble::tribble(
-    ~year, ~sector, ~technology, ~sector_unit_ds, ~price_unit_iea, ~price_unit_etr, ~B2DS, ~b2ds_source, ~NPS, ~nps_source, ~SDS, ~sds_source, ~Baseline, ~baseline_source,
-    2020, "Power", "HydroCap", "MW", "Dollars per MwH", "USD per MWh", NA_real_, "test_source", 10, "test_source", 10, "test_source", 10, "custom",
-    2030, "Coal", "Coal", "TPA", "US$2010/GJ", "Dollars per ton", 10, "test_source", 10, "test_source", 10, "test_source", 10, "custom"
-  )
+            test_data_prices <- tibble::tribble(
+              ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
+              2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
+              2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
+            )
 
-  test_data_prices %>%
-    readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
+            test_data_prices %>%
+              readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
 
-  test_input_path <- file.path(tempdir(), "prices_data_input.csv")
+            test_input_path <- file.path(tempdir(), "prices_data_input.csv")
 
-  test_data <- read_price_data_internal_old(path = test_input_path)
+            test_data <- read_price_data_internal_old(path = test_input_path)
 
-  testthat::expect_s3_class(test_data, "data.frame")
+            testthat::expect_s3_class(test_data, "data.frame")
 
-  unlink(file.path(tempdir(), "prices_data_input.csv"))
-})
+            unlink(file.path(tempdir(), "prices_data_input.csv"))
+          })
