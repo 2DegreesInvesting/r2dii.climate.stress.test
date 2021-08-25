027fde01e73ce426d8e85480ce28a23526f40579
MirijaHa <mirja@2degrees-investing.org>
2021-08-25 10:40:56 +0200

    Revert "more removal for test"
    This reverts commit 0f9f551f11fd8166f53c3f52270de4796b14e1bf.

--- tests/testthat/test-read_price_data_internal.R
+++ tests/testthat/test-read_price_data_internal.R
@@ -1,27 +1,27 @@
-# test_that("without specified arguments, read_price_data_internal() throws
-#           error", {
-#   testthat::expect_error(
-#     read_price_data_internal(),
-#     "Must provide 'path'"
-#   )
-# })
-#
-# test_that("with valid arguments set, read_price_data_internal() returns
-#           data.frame", {
-#   test_data_prices <- tibble::tribble(
-#     ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
-#     2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
-#     2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
-#   )
-#
-#   test_data_prices %>%
-#     readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
-#
-#   test_input_path <- file.path(tempdir(), "prices_data_input.csv")
-#
-#   test_data <- read_price_data_internal(path = test_input_path)
-#
-#   testthat::expect_s3_class(test_data, "data.frame")
-#
-#   unlink(file.path(tempdir(), "prices_data_input.csv"))
-# })
+test_that("without specified arguments, read_price_data_internal() throws
+          error", {
+  testthat::expect_error(
+    read_price_data_internal(),
+    "Must provide 'path'"
+  )
+})
+
+test_that("with valid arguments set, read_price_data_internal() returns
+          data.frame", {
+  test_data_prices <- tibble::tribble(
+    ~year, ~source, ~scenario, ~scenario_geography, ~technology, ~indicator, ~unit, ~price,
+    2020, "WEO2020", "SDS", "United States", "Gas", "price", "usd/Mbtu", 10,
+    2030, "WEO2020", "SDS", "United States", "RenewablesCap", "LCOE", "$/MWh", 10
+  )
+
+  test_data_prices %>%
+    readr::write_csv(file.path(tempdir(), "prices_data_input.csv"))
+
+  test_input_path <- file.path(tempdir(), "prices_data_input.csv")
+
+  test_data <- read_price_data_internal(path = test_input_path)
+
+  testthat::expect_s3_class(test_data, "data.frame")
+
+  unlink(file.path(tempdir(), "prices_data_input.csv"))
+})
