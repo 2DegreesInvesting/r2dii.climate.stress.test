fe531d20256a6dc919dba6e9dfe5a7c2e9d57c46
MirijaHa <mirja@2degrees-investing.org>
2021-08-27 10:25:19 +0200

    disambiguate names

--- tests/testthat/test-convert_cap_to_generation.R
+++ tests/testthat/test-convert_cap_to_generation.R
@@ -47,19 +47,19 @@ test_that("multiplies the power capacity by hours per year and the capacity
 })
 
 # convert_power_cap_to_generation -----------------------------------------
-capacity_factors <- tibble::tibble(technology = c(rep("HydroCap", 8), rep("NuclearCap", 8)),
+new_capacity_factors <- tibble::tibble(technology = c(rep("HydroCap", 8), rep("NuclearCap", 8)),
                                    scenario_geography = c(rep(c(rep("Global", 4), rep("Brazil", 4)), 2)),
                                    scenario = c(rep(c(rep("NPS", 2), rep("SDS", 2)), 4)),
                                    year = c(rep(c(2021, 2022), 8)),
                                    capacity_factor = c(rep(c(rep(0.5, 2), rep(0.3, 2)), 4)))
 
-# reducing test_data to data with matching years in capacity_factors
-test_data <- read_test_data("convert_cap_to_generation.csv") %>%
+# reducing new_test_data to data with matching years in new_capacity_factors
+new_test_data <- read_test_data("convert_cap_to_generation.csv") %>%
   dplyr::filter(year %in% c(2021, 2022))
 
 test_that("error is thrown if baseline scenario is missing from capacity_factors_power", {
-  testthat::expect_error(convert_power_cap_to_generation(data = test_data,
-                                                         capacity_factors_power = capacity_factors,
+  testthat::expect_error(convert_power_cap_to_generation(data = new_test_data,
+                                                         capacity_factors_power = new_capacity_factors,
                                                          baseline_scenario = "NOT_A_SCENARIO"),
                          "Baseline Scenario NOT_A_SCENARIO is missing")
 
@@ -67,8 +67,8 @@ test_that("error is thrown if baseline scenario is missing from capacity_factors
 })
 
 test_that("plan_tech_prod and scen_tech_prod are equal (assuming equals values before funcion is applied) if capacity factors do not differ between scenarios.", {
-  test_results <- convert_power_cap_to_generation(data = test_data,
-                                                  capacity_factors_power = capacity_factors %>%
+  test_results <- convert_power_cap_to_generation(data = new_test_data,
+                                                  capacity_factors_power = new_capacity_factors %>%
                                                     dplyr::mutate(capacity_factor = 0.5),
                                                   baseline_scenario = "NPS")
 
@@ -77,8 +77,8 @@ test_that("plan_tech_prod and scen_tech_prod are equal (assuming equals values b
           })
 
 test_that("plan_tech_prod and scen_tech_prod differ (assuming equals values before funcion is applied) only for power sector for not baseline scneario if capacity factors differ between scenarios.", {
-  test_results <- convert_power_cap_to_generation(data = test_data,
-                                                  capacity_factors_power = capacity_factors,
+  test_results <- convert_power_cap_to_generation(data = new_test_data,
+                                                  capacity_factors_power = new_capacity_factors,
                                                   baseline_scenario = "NPS")
 
   power_results_SDS <- test_results %>% dplyr::filter(ald_sector == "Power" & scenario == "SDS")
@@ -92,8 +92,8 @@ testthat::expect_true(all(other_sector_results$plan_tech_prod == other_sector_re
 
 test_that("multiplication results differ by further variables", {
   # picked only year here to keep test manageable
-  test_results <- convert_power_cap_to_generation(data = test_data,
-                                                  capacity_factors_power = capacity_factors %>%
+  test_results <- convert_power_cap_to_generation(data = new_test_data,
+                                                  capacity_factors_power = new_capacity_factors %>%
                                                     dplyr::mutate(capacity_factor = dplyr::if_else(.data$year == 2022, .data$capacity_factor - 0.1, .data$capacity_factor)),
                                                   baseline_scenario = "NPS")
 
