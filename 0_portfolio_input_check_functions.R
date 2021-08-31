04676e6a9912558421416da0fdb8309da2645c5f
MirijaHa <mirja@2degrees-investing.org>
2021-08-31 12:00:33 +0200

    drop function 29

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -216,23 +216,6 @@ check_funds_wo_bbg <- function(fund_data, fin_data){
 }
 
 ###
-add_flags <- function(portfolio){
-
-  portfolio <- portfolio %>%
-    mutate(flag = case_when(
-
-      !has_currency ~ "Missing currency information",
-      !has_valid_input ~ "Negative or missing input value",
-      !has_valid_isin ~ "Invalid or missing ISIN",
-      !has_bbg_data ~ "Holding not in Bloomberg database",
-
-      TRUE ~ "Included in analysis"
-    ))
-
-  portfolio
-
-}
-
 overall_validity_flag <- function(portfolio_total){
 
   portfolio_total <- portfolio_total %>%
