abe624914e3445e1571534c00f66b6723da8361f
MirijaHa <mirja@2degrees-investing.org>
2021-08-31 12:02:34 +0200

    drop function 32

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -216,14 +216,6 @@ check_funds_wo_bbg <- function(fund_data, fin_data){
 }
 
 ###
-calculate_number_of_shares <- function(portfolio){
-
-  portfolio <- portfolio %>%
-    mutate(number_of_shares = ifelse(is.na(number_of_shares) & asset_type == "Equity", value_usd/unit_share_price, number_of_shares))
-
-  return(portfolio)
-}
-
 create_id_columns <- function(portfolio, portfolio_type){
 
   if(portfolio_type == "Equity"){
