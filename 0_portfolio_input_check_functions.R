b3b30154eff485c094831dcaae16375f84265f87
MirijaHa <mirja@2degrees-investing.org>
2021-08-31 12:03:08 +0200

    drop function 33

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -215,28 +215,6 @@ check_funds_wo_bbg <- function(fund_data, fin_data){
 
 }
 
-###
-create_id_columns <- function(portfolio, portfolio_type){
-
-  if(portfolio_type == "Equity"){
-
-    portfolio <- portfolio %>%
-      rename(id = bloomberg_id) %>%
-      mutate(id_name = "bloomberg_id",
-             id = as.character(id))
-  }
-  if(portfolio_type == "Bonds"){
-
-    portfolio <- portfolio %>%
-      rename(id = corporate_bond_ticker) %>%
-      mutate(id_name = "corporate_bond_ticker",
-             id = as.character(id))
-  }
-
-  return(portfolio)
-
-}
-
 # FINAL SCRIPTS
 get_and_clean_fin_data <- function(fund_data){
 
