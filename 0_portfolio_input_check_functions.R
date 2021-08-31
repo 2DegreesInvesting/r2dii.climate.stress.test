3a9d69f331145576d748a6b0149aff492edb1c62
MirijaHa <mirja@2degrees-investing.org>
2021-08-31 11:58:42 +0200

    drop function 26

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -218,20 +218,6 @@ check_funds_wo_bbg <- function(fund_data, fin_data){
 ###
 
 # Add Columns for missing or incorrect information
-check_missing_currency <- function(portfolio_total){
-
-  # Currency blank or not in our currency data frame
-  portfolio_total <- portfolio_total %>%
-    mutate(has_currency = case_when(
-      is.na(currency) ~ FALSE,
-      currency == "" ~ FALSE,
-      !currency %in% currencies$currency ~ FALSE,
-      TRUE ~ TRUE)
-    )
-
-  portfolio_total
-}
-
 check_valid_input_value <- function(portfolio_total){
 
   # Currency negative or missing market value/number of shares
