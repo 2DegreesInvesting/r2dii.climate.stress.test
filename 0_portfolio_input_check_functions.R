90afe7a1176c9995ff1cd91da20b50ec283a00df
MirijaHa <mirja@2degrees-investing.org>
2021-08-31 12:01:19 +0200

    drop function 30

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -216,22 +216,6 @@ check_funds_wo_bbg <- function(fund_data, fin_data){
 }
 
 ###
-overall_validity_flag <- function(portfolio_total){
-
-  portfolio_total <- portfolio_total %>%
-    mutate(valid_input = case_when(
-      !has_currency ~ FALSE,
-      !has_bbg_data ~ FALSE,
-      !has_valid_input ~ FALSE,
-      !has_valid_isin ~ FALSE,
-      TRUE ~ TRUE
-
-    ))
-
-  portfolio_total
-
-}
-
 check_for_ald <- function(portfolio_subset, portfolio_type, relevant_fin_data){
 
   if (data_check(portfolio_subset)){
