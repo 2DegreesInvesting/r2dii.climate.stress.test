bf324f06a9b51ddb231ae7a9bccbd650420245a4
MirijaHa <mirja@2degrees-investing.org>
2021-09-20 13:35:27 +0200

    add draft for missing checks

--- R/utils.R
+++ R/utils.R
@@ -156,14 +156,30 @@ validate_data_has_expected_cols <- function(data,
   stopifnot(data_has_expected_columns)
 }
 
-
-check_duplicate_rows <- function(input_data, composite_unique_cols) {
+check_row_consistency <- function(input_data, composite_unique_cols) {
   input_data %>%
+    report_missings_col_combinations(cols = composite_unique_cols) %>%
     report_and_remove_duplicates(cols = names(input_data)) %>%
     report_and_remove_duplicates(cols = composite_unique_cols)
 }
 
+report_missings_col_combinations <- function(input_data, composite_unique_cols) {
+
+  all_combinations <- input_data %>%
+    tidyr::expand(!!!dplyr::sym(composite_unique_cols))
+
+  missing_rows <- all_combinations %>%
+    dplyr::anti_join(input_data, by = !!!dplyr::sym(composite_unique_cols))
+
+
+  if (nrow(missing_rows) > 0) {
+    warning(paste0("Identified ", nrow(missing_rows), " missing combinations on columns ", paste(missing_rows, sep = ","), "."))
+  }
+
+}
+
 report_and_remove_duplicates <- function(input_data, cols) {
+
   duplicates <- input_data %>%
     dplyr::group_by(!!!dplyr::sym(cols)) %>%
     dplyr::filter(dplyr::n() > 1) %>%
@@ -180,8 +196,5 @@ report_and_remove_duplicates <- function(input_data, cols) {
     stop(paste0("No rows remaining after removing duplicates on columns ", paste(cols, sep = ","), "."))
   }
   return(input_data_without_duplicates)
-}
-
-check_missing_rows <- function(input_data, composite_unique_cols) {
 
 }
