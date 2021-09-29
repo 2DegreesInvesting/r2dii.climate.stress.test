cd64658d2101ac891ec93c270422c4a38305b5fe
MirijaHa <mirja@2degrees-investing.org>
2021-09-29 17:17:20 +0200

    fixed test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -131,9 +131,9 @@ test_that("Warnings are thrown for dataset with full and partial duplicates", {
     c = c(1, 1, 2, 3)
   )
 
-  expect_warning(report_all_duplicate_kinds(
+  expect_warning(expect_warning(report_all_duplicate_kinds(
     data = data,
     composite_unique_cols = c("a", "b")
-  ))
+  )))
 
 })
