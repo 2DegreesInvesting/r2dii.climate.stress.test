bc5cead98aade40bb28c51bec5694e2633e5656a
MirijaHa <mirja@2degrees-investing.org>
2021-09-30 18:15:18 +0200

    add doc

--- R/calculate_aum.R
+++ R/calculate_aum.R
@@ -1,3 +1,13 @@
+#' Calculate aum
+#'
+#' Function calculated asset under management for portfolio from sector
+#' exposures.
+#'
+#' @param sector_exposures A tibble holding sector exposure.
+#'
+#' @return A tibble holding columns investor_name, portfolio_name and
+#'   asset_portfolio_value.
+#' @export
 calculate_aum <- function(sector_exposures) {
   aum <- sector_exposures %>%
     group_by(investor_name, portfolio_name) %>%
