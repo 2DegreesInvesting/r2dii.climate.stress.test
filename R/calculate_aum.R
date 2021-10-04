9f4c47572551d6550a2b229120e9fb6372c4355c
MirijaHa <mirja@2degrees-investing.org>
2021-10-04 09:10:40 +0200

    set groups to drop to ungroup

--- R/calculate_aum.R
+++ R/calculate_aum.R
@@ -13,7 +13,7 @@ calculate_aum <- function(sector_exposures) {
     group_by(investor_name, portfolio_name) %>%
     summarise(
       asset_portfolio_value = sum(valid_value_usd),
-      .groups = "drop_last"
+      .groups = "drop"
     )
   return(aum)
 }
