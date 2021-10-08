9d7b6ec48e318793a4d3ca491655107f3f5e7a81
MirijaHa <mirja@2degrees-investing.org>
2021-10-08 11:28:12 +0200

    add functionality

--- R/filter_data.R
+++ R/filter_data.R
@@ -97,9 +97,25 @@ apply_filters <- function(data,
 #'
 #' @return Input tibble with potentially removed rows.
 filter_negative_late_and_sudden <- function(data_with_late_and_sudden) {
+  negative_late_and_sudden <- data_with_late_and_sudden %>%
+    dplyr::filter(late_and_sudden < 0) %>%
+    dplyr::select(company_name, technology) %>%
+    dplyr::distinct()
 
-  if (nrow(ddata_with_late_and_sudden) == 0) {
-    stop("No rows remain after removing negative late and sudden trajectories.")
+  if (nrow(negative_late_and_sudden) > 0) {
+    n_rows_before_removal <- nrow(data_with_late_and_sudden)
+
+    data_with_late_and_sudden <-
+      data_with_late_and_sudden %>%
+      dplyr::anti_join(negative_late_and_sudden, by = c("company_name", "technology"))
+
+    cat(paste0("Removed ", n_rows_before_removal - nrow(data_with_late_and_sudden),
+               " rows due to negative late and sudden targets."))
+
+    if (nrow(data_with_late_and_sudden) == 0) {
+      stop("No rows remain after removing negative late and sudden trajectories.")
+    }
   }
 
+  return(data_with_late_and_sudden)
 }
