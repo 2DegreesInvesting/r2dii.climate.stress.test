1df99b4763df55f32217efaf5724b2f2a077ed01
MirijaHa <mirja@2degrees-investing.org>
2021-10-08 11:18:12 +0200

    improve test cases

--- tests/testthat/test-filter_data.R
+++ tests/testthat/test-filter_data.R
@@ -88,6 +88,29 @@ test_that("technology x company_name combinations that hold at least 1 negative
 
   filtered_data <- filter_negative_late_and_sudden(input_data)
 
-  expect_equal(input_data %>% dplyr::filter(!(company_name == "firm" & technology <- "some")), filtered_data)
+  expect_equal(input_data %>% dplyr::filter(!(company_name == "firm" & technology == "some")), filtered_data)
 })
 
+test_that("removal works if several company_name x technology combinations are affected", {
+  input_data <- tibble::tibble(
+    company_name = c("firm", "firm", "firm", "biz", "biz"),
+    technology = c("some", "some", "other", "some", "other"),
+    late_and_sudden = c(-1, 1, -1, -1, 1),
+    some_col = rep("sth", 5)
+  )
+
+  filtered_data <- filter_negative_late_and_sudden(input_data)
+
+  expect_equal(input_data %>% dplyr::filter(company_name == "biz" & technology == "other"), filtered_data)
+})
+
+test_that("error is thrown if no rows remain", {
+  input_data <- tibble::tibble(
+    company_name = c("firm", "firm", "firm", "biz", "biz"),
+    technology = c("some", "some", "other", "some", "other"),
+    late_and_sudden = rep(-1, 5),
+    some_col = rep("sth", 5)
+  )
+
+  expect_error(filtered_data <- filter_negative_late_and_sudden(input_data), "No rows remain")
+})
