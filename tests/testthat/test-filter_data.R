6c34767d6940da952d4253d98d2dbff7c8eda044
MirijaHa <mirja@2degrees-investing.org>
2021-10-08 11:04:30 +0200

    add tests

--- tests/testthat/test-filter_data.R
+++ tests/testthat/test-filter_data.R
@@ -63,5 +63,31 @@ test_that("apply_filters returns expected number of rows based on input filter
 })
 
 # filter_negative_late_and_sudden -----------------------------------------
+test_that("input remains unchanged if no negative late_and_sudden levels are
+          present", {
+  input_data <- tibble::tibble(
+    company_name = c("firm", "firm", "biz", "biz"),
+    technology = c("some", "other", "some", "other"),
+    late_and_sudden = 1:4,
+    some_col = rep("sth", 4)
+  )
 
+  filtered_data <- filter_negative_late_and_sudden(input_data)
+
+  expect_equal(input_data, filtered_data)
+})
+
+test_that("technology x company_name combinations that hold at least 1 negative
+          value on late_and_sudden are removed", {
+  input_data <- tibble::tibble(
+    company_name = c("firm", "firm", "firm", "biz", "biz"),
+    technology = c("some", "some", "other", "some", "other"),
+    late_and_sudden = c(-1, 1, 1, 0, 1),
+    some_col = rep("sth", 5)
+  )
+
+  filtered_data <- filter_negative_late_and_sudden(input_data)
+
+  expect_equal(input_data %>% dplyr::filter(!(company_name == "firm" & technology <- "some")), filtered_data)
+})
 
