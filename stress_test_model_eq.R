d447a5044b3837dfb5fec2465fb7c68ecf60bb0b
MirijaHa <mirja@2degrees-investing.org>
2021-10-11 14:20:59 +0200

    indent

--- stress_test_model_eq.R
+++ stress_test_model_eq.R
@@ -470,57 +470,57 @@ for (i in seq(1, nrow(transition_scenarios))) {
     cols = names(plan_carsten_equity)
   )
 
-    equity_results <- dplyr::bind_rows(
-      equity_results,
-      company_asset_value_at_risk(
-        data = equity_annual_profits,
-        terminal_value = terminal_value,
-        shock_scenario = shock_scenario,
-        div_netprofit_prop_coef = div_netprofit_prop_coef,
-        plan_carsten = plan_carsten_equity,
-        port_aum = equity_port_aum,
-        flat_multiplier = 1,
-        exclusion = excluded_companies
-      )
+  equity_results <- dplyr::bind_rows(
+    equity_results,
+    company_asset_value_at_risk(
+      data = equity_annual_profits,
+      terminal_value = terminal_value,
+      shock_scenario = shock_scenario,
+      div_netprofit_prop_coef = div_netprofit_prop_coef,
+      plan_carsten = plan_carsten_equity,
+      port_aum = equity_port_aum,
+      flat_multiplier = 1,
+      exclusion = excluded_companies
+    )
+  )
+
+  equity_overall_pd_changes <- equity_annual_profits %>%
+    calculate_pd_change_overall(
+      shock_year = transition_scenario_i$year_of_shock,
+      end_of_analysis = end_year,
+      exclusion = excluded_companies,
+      risk_free_interest_rate = risk_free_rate
     )
 
-    equity_overall_pd_changes <- equity_annual_profits %>%
-      calculate_pd_change_overall(
-        shock_year = transition_scenario_i$year_of_shock,
-        end_of_analysis = end_year,
-        exclusion = excluded_companies,
-        risk_free_interest_rate = risk_free_rate
-      )
+  # TODO: ADO 879 - note which companies produce missing results due to
+  # insufficient input information (e.g. NAs for financials or 0 equity value)
 
-    # TODO: ADO 879 - note which companies produce missing results due to
-    # insufficient input information (e.g. NAs for financials or 0 equity value)
-
-    equity_expected_loss <- dplyr::bind_rows(
-      equity_expected_loss,
-      company_expected_loss(
-        data = equity_overall_pd_changes,
-        loss_given_default = lgd_subordinated_claims, # TODO: which one?
-        exposure_at_default = plan_carsten_equity,
-        port_aum = equity_port_aum
-      )
+  equity_expected_loss <- dplyr::bind_rows(
+    equity_expected_loss,
+    company_expected_loss(
+      data = equity_overall_pd_changes,
+      loss_given_default = lgd_subordinated_claims, # TODO: which one?
+      exposure_at_default = plan_carsten_equity,
+      port_aum = equity_port_aum
     )
+  )
 
-    # TODO: ADO 879 - note which companies produce missing results due to
-    # insufficient output from overall pd changes or related financial data inputs
+  # TODO: ADO 879 - note which companies produce missing results due to
+  # insufficient output from overall pd changes or related financial data inputs
 
-    equity_annual_pd_changes <- dplyr::bind_rows(
-      equity_annual_pd_changes,
-      calculate_pd_change_annual(
-        data = equity_annual_profits,
-        shock_year = transition_scenario_i$year_of_shock,
-        end_of_analysis = end_year,
-        exclusion = excluded_companies,
-        risk_free_interest_rate = risk_free_rate
-      )
+  equity_annual_pd_changes <- dplyr::bind_rows(
+    equity_annual_pd_changes,
+    calculate_pd_change_annual(
+      data = equity_annual_profits,
+      shock_year = transition_scenario_i$year_of_shock,
+      end_of_analysis = end_year,
+      exclusion = excluded_companies,
+      risk_free_interest_rate = risk_free_rate
     )
+  )
 
-    # TODO: ADO 879 - note which companies produce missing results due to
-    # insufficient input information (e.g. NAs for financials or 0 equity value)
+  # TODO: ADO 879 - note which companies produce missing results due to
+  # insufficient input information (e.g. NAs for financials or 0 equity value)
 
 }
 
