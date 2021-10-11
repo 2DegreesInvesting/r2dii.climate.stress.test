4c88a869aebb0aef35e99327b03d698f311d77a6
MirijaHa <mirja@2degrees-investing.org>
2021-10-11 14:25:50 +0200

    remove unneeded condition

--- stress_test_model_cb.R
+++ stress_test_model_cb.R
@@ -469,51 +469,6 @@ for (i in seq(1, nrow(transition_scenarios))) {
     cols = names(plan_carsten_bonds)
   )
 
-  if (!exists("excluded_companies")) {
-    bonds_results <- dplyr::bind_rows(
-      bonds_results,
-      company_asset_value_at_risk(
-        data = bonds_annual_profits,
-        terminal_value = terminal_value,
-        shock_scenario = shock_scenario,
-        div_netprofit_prop_coef = div_netprofit_prop_coef,
-        plan_carsten = plan_carsten_bonds,
-        port_aum = bonds_port_aum,
-        flat_multiplier = 0.15,
-        exclusion = NULL
-      )
-    )
-
-    bonds_overall_pd_changes <- bonds_annual_profits %>%
-      calculate_pd_change_overall(
-        shock_year = transition_scenario_i$year_of_shock,
-        end_of_analysis = end_year,
-        exclusion = NULL,
-        risk_free_interest_rate = risk_free_rate
-      )
-
-    bonds_expected_loss <- dplyr::bind_rows(
-      bonds_expected_loss,
-      company_expected_loss(
-        data = bonds_overall_pd_changes,
-        loss_given_default = lgd_subordinated_claims,
-        exposure_at_default = plan_carsten_bonds,
-        port_aum = bonds_port_aum
-      )
-    )
-
-    bonds_annual_pd_changes <- dplyr::bind_rows(
-      bonds_annual_pd_changes,
-      calculate_pd_change_annual(
-        data = bonds_annual_profits,
-        shock_year = transition_scenario_i$year_of_shock,
-        end_of_analysis = end_year,
-        exclusion = NULL,
-        risk_free_interest_rate = risk_free_rate
-      )
-    )
-
-  } else {
     bonds_results <- dplyr::bind_rows(
       bonds_results,
       company_asset_value_at_risk(
@@ -562,10 +517,8 @@ for (i in seq(1, nrow(transition_scenarios))) {
         risk_free_interest_rate = risk_free_rate
       )
     )
-
     # TODO: ADO 879 - note which companies produce missing results due to
     # insufficient input information (e.g. NAs for financials or 0 equity value)
-  }
 }
 
 # Output bonds results
