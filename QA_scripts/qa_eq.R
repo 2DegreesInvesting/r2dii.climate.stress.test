a1cd4f45cf8dab9224d570c97a1b5b52fc513f4c
jacobvjk <jacob.kastl@gmail.com>
2021-10-12 13:08:08 +0200

    update eq qa

--- QA_scripts/qa_eq.R
+++ QA_scripts/qa_eq.R
@@ -94,3 +94,59 @@ sum_carbon_budgets_eq <- qa_annual_profits_eq %>%
     scenario_name_qa = "Carbon balance 2030",
     cumulative = TRUE
   )
+
+
+# credit risk QA graphs--------------------------------------------------------
+
+# ... overall change of credit risk graphs-------------------------------------
+plot_pd_change_company_sector <- equity_expected_loss %>%
+  overall_pd_change_company_sector(
+    shock_year = 2030,
+    sector_filter = c("Power", "Automotive"),
+    company_filter = c("Daimler Ag", "Enel Spa"),
+    geography_filter = scenario_geography_filter
+  )
+
+ggplot2::ggsave(
+  "plot_pd_change_company_sector.png",
+  plot = plot_pd_change_company_sector,
+  path = r2dii.utils::path_dropbox_2dii(outputs_path)
+)
+
+plot_pd_change_shock_year_sector <- equity_overall_pd_changes_sector %>%
+  overall_pd_change_sector_shock_year(
+    scenario_filter = c("Carbon balance 2025", "Carbon balance 2030", "Carbon balance 2035"),
+    geography_filter = scenario_geography_filter
+  )
+
+ggplot2::ggsave(
+  "plot_pd_change_shock_year_sector.png",
+  plot = plot_pd_change_shock_year_sector,
+  path = r2dii.utils::path_dropbox_2dii(outputs_path)
+)
+
+# ... annual change of credit risk graphs--------------------------------------
+plot_annual_pd_change_company_sector <- equity_annual_pd_changes %>%
+  annual_pd_change_company_sector(
+    shock_year = 2030,
+    company_filter = c("Daimler Ag", "Enel Spa", "Total Sa"),
+    geography_filter = scenario_geography_filter
+  )
+
+ggplot2::ggsave(
+  "plot_annual_pd_change_company_sector.png",
+  plot = plot_annual_pd_change_company_sector,
+  path = r2dii.utils::path_dropbox_2dii(outputs_path)
+)
+
+plot_annual_pd_change_shock_year_sector <- equity_annual_pd_changes_sector %>%
+  annual_pd_change_sector_shock_year(
+    shock_year_filter = c(2025, 2030, 2035),
+    geography_filter = scenario_geography_filter
+  )
+
+ggplot2::ggsave(
+  "plot_annual_pd_change_shock_year_sector.png",
+  plot = plot_annual_pd_change_shock_year_sector,
+  path = r2dii.utils::path_dropbox_2dii(outputs_path)
+)
