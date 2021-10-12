92d36125e41d99c1af42bb76728a6d186b3ec669
MirijaHa <88371355+MirijaHa@users.noreply.github.com>
2021-10-12 11:35:25 +0200

    Simplify merton (#78)

---
+++ R/calc_survival_probability_merton.R
@@ -0,0 +1,48 @@
+#' Calculate survival probability
+#'
+#' Function calculates survival probability for a maturity based on a structural
+#' Merton model.
+#' For details on implementation please compare [CreditRisk::Merton()].
+#' Unlike [CreditRisk::Merton()] this implementation:
+#' 1. only holds functionality to calculate probability of survival
+#' 1. can be called in vectorised fashion
+#' 1. has additional input validation by checking that all input values are
+#' non negative numeric vectors of the same length.
+#'
+#' @param L Numeric vector, holding debt values at maturity.
+#' @param V0 Numeric vector, holding company values at time t0.
+#' @param sigma Numeric vector, holding volatility values.
+#' @param r Numeric vector, holding risk free interest rates.
+#' @param t Vector vector holding debt maturities.
+#'
+#' @return A tibble holding survival probabilities,
+calc_survival_probabily_merton <- function(L, V0, sigma, r, t) {
+  input_args <- list(L, V0, sigma, r, t)
+
+  if (dplyr::n_distinct(purrr::map_int(input_args, length)) > 1) {
+    stop("All input arugments need to have the same length.")
+  }
+
+  if (!all(unique(purrr::map_lgl(input_args, is.numeric)))) {
+    stop("All input arguments need to be numeric.")
+  }
+
+  if (!all(unique(purrr::map_lgl(input_args, function(x) {
+    all(x >= 0)
+  })))) {
+    stop("Input arguments may not hold negative numbers.")
+  }
+
+  d1 <- (log(V0 / L) + (r + (sigma^2 / 2) * t)) / (sigma * sqrt(t))
+  d2 <- d1 - sigma * sqrt(t)
+
+  # Default Probability
+  p_default <- stats::pnorm(-d2)
+
+  # Survival Probability Q(tau > T)
+  p_survival <- 1 - p_default
+
+  survival <- tibble::tibble(Survival = p_survival)
+
+  return(survival)
+}
