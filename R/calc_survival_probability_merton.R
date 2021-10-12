69aef1b93425fc270662389bb582b000173739f7
MirijaHa <mirja@2degrees-investing.org>
2021-10-12 08:54:28 +0200

    fix validation bug

--- R/calc_survival_probability_merton.R
+++ R/calc_survival_probability_merton.R
@@ -18,9 +18,9 @@
 #' @return A tibble holding survival probabilities,
 calc_survival_probabily_merton <- function(L, V0, sigma, r, t) {
 
-  input_args <- c(L, V0, sigma, r, t)
+  input_args <- list(L, V0, sigma, r, t)
 
-  if (dplyr::n_distinct(length(input_args)) > 1) {
+  if (dplyr::n_distinct(purrr::map_int(input_args, length)) > 1) {
     stop("All input arugments need to have the same length.")
   }
 
