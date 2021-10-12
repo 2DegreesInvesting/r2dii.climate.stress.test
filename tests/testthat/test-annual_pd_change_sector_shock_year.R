6553b91450a06df17bc16ba68e242e970178c98a
jacobvjk <jacob.kastl@gmail.com>
2021-10-12 13:14:17 +0200

    adapt tests

--- tests/testthat/test-annual_pd_change_sector_shock_year.R
+++ tests/testthat/test-annual_pd_change_sector_shock_year.R
@@ -1,17 +1,17 @@
 test_that("without specified arguments, function throws error", {
   testthat::expect_error(
-    annual_pd_change_technology_shock_year(),
+    annual_pd_change_sector_shock_year(),
     "argument \"data\" is missing"
   )
 })
 
 test_that("production trajectories function returns a ggplot object", {
-  test_input_graph <- read_test_data("graph_annual_pd_change_technology_shock_year.csv")
+  test_input_graph <- read_test_data("graph_annual_pd_change_sector_shock_year.csv")
 
   test_shock_year <- 2030
   test_geography_filter <- "Global"
 
-  test_plot <- annual_pd_change_technology_shock_year(
+  test_plot <- annual_pd_change_sector_shock_year(
     data = test_input_graph,
     shock_year = test_shock_year,
     geography_filter = test_geography_filter
