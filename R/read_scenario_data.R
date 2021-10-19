b45d441e821418fd7ae6d1096d5fb76f065c08e8
MirijaHa <88371355+MirijaHa@users.noreply.github.com>
2021-10-19 12:30:50 +0200

    Switch analysis input (#94)

---
+++ R/read_scenario_data.R
@@ -0,0 +1,30 @@
+#' Read in scenario data
+#'
+#' Function reads in scenario data and checks for existence of required columns.
+#'
+#' @param path Path to dir holding scenario data.
+#'
+#' @return A tibble holding scenario data.
+read_scenario_data <- function(path) {
+  scenario_data <- readr::read_csv(path, col_types = readr::cols(
+    scenario_source = "c",
+    scenario_geography = "c",
+    scenario = "c",
+    ald_sector = "c",
+    units = "c",
+    technology = "c",
+    year = "d",
+    direction = "c",
+    fair_share_perc = "d"
+  ))
+
+  validate_data_has_expected_cols(
+    data = scenario_data,
+    expected_columns = c(
+      "scenario_source", "scenario_geography", "scenario",
+      "ald_sector", "units", "technology", "year",
+      "direction", "fair_share_perc"
+    )
+  )
+  return(scenario_data)
+}
