e68b07fb70e6bc3f22a71a56e4ad7fc415262588
jacobvjk <jacob.kastl@gmail.com>
2021-10-20 11:23:25 +0200

    add test for new generation of shock scenarios

--- tests/testthat/test-read_transition_scenarios.R
+++ tests/testthat/test-read_transition_scenarios.R
@@ -49,3 +49,91 @@ test_that("with all arguments set, and valid shock years
 
   testthat::expect_s3_class(test_data_new, "data.frame")
 })
+
+test_that("without specified arguments, generate_transition_shocks throws
+          error", {
+  testthat::expect_error(
+    generate_transition_shocks(),
+    "no default"
+  )
+})
+
+test_that("with invalid shock years, generate_transition_shocks throws error", {
+  test_start <- 2019
+  test_end <- 2040
+  test_shock_years <- c(2010)
+
+  testthat::expect_error(
+    generate_transition_shocks(
+      start_of_analysis = test_start,
+      end_of_analysis = test_end,
+      shock_years = test_shock_years
+    ),
+    "Year of shock out of bounds"
+  )
+})
+
+test_that("with wrong input type, generate_transition_shocks throws error", {
+  test_start <- "2019"
+  test_end <- 2040
+  test_shock_years <- c(2025, 2030)
+
+  testthat::expect_error(
+    generate_transition_shocks(
+      start_of_analysis = test_start,
+      end_of_analysis = test_end,
+      shock_years = test_shock_years
+    ),
+    "numeric"
+  )
+})
+
+test_that("with wrong input length for start_of_analysis or end_of_analysis,
+          generate_transition_shocks throws error", {
+  test_start <- c(2019, 2025)
+  test_end <- 2040
+  test_shock_years <- c(2025, 2030)
+
+  testthat::expect_error(
+    generate_transition_shocks(
+      start_of_analysis = test_start,
+      end_of_analysis = test_end,
+      shock_years = test_shock_years
+    ),
+    "length 1"
+  )
+
+  test_start <- 2019
+  test_end <- c(2035, 2040)
+  test_shock_years <- c(2025, 2030)
+
+  testthat::expect_error(
+    generate_transition_shocks(
+      start_of_analysis = test_start,
+      end_of_analysis = test_end,
+      shock_years = test_shock_years
+    ),
+    "length 1"
+  )
+})
+
+test_that("with valid inputs, generate_transition_shocks generates a data frame
+          with number of rows equal to number of shock years", {
+  test_start <- 2019
+  test_end <- 2040
+  test_shock_years <- 2025:2030
+
+  n_shock_years <- length(test_shock_years)
+
+  shock_scenarios <- generate_transition_shocks(
+    start_of_analysis = test_start,
+    end_of_analysis = test_end,
+    shock_years = test_shock_years
+  )
+
+  testthat::expect_equal(
+    nrow(shock_scenarios),
+    n_shock_years
+  )
+})
+
