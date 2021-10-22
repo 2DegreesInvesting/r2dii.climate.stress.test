7700970d15d195cfaa7253c674fc3a36cffd9e06
MirijaHa <mirja@2degrees-investing.org>
2021-10-22 10:42:23 +0200

    for excluded companies

--- R/check_and_filter_data.R
+++ R/check_and_filter_data.R
@@ -6,9 +6,16 @@ check_and_filter_data <- function(st_data_list, start_year, end_year, scenarios_
     dplyr::filter(.data$technology %in% technologies_lookup) %>%
     dplyr::filter(dplyr::between(.data$year, start_year, end_year))
 
+  excluded_companies_filtered <- st_data_list$excluded_companies %>%
+    dplyr::filter(.data$technology %in% technologies_lookup)
+
   report_all_duplicate_kinds(
     data = capacity_factors_power_filtered,
     composite_unique_cols = c("scenario", "scenario_geography", "technology", "year"))
+
+  report_all_duplicate_kinds(
+    data = excluded_companies_filtered,
+    composite_unique_cols = c("company_name", "technology"))
 
   return(list(
     capacity_factors_power = capacity_factors_power_filtered,
