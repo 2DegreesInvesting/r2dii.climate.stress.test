efaf9d08c8cb1db873db026b9232c7278ddce933
MirijaHa <mirja@2degrees-investing.org>
2021-10-22 11:30:06 +0200

    add scen data

--- R/check_and_filter_data.R
+++ R/check_and_filter_data.R
@@ -14,6 +14,13 @@ check_and_filter_data <- function(st_data_list, start_year, end_year, scenarios_
     dplyr::filter(.data$technology %in% technologies_lookup) %>%
     dplyr::filter(dplyr::between(.data$year, start_year, end_year))
 
+  scenario_data_filtered <- st_data_list$scenario_data %>%
+    dplyr::filter(.data$scenario %in% scenarios_filter) %>%
+    dplyr::filter(.data$scenario_geography %in% scenario_geography_filter) %>%
+    dplyr::filter(.data$ald_sector %in% sectors_lookup) %>%
+    dplyr::filter(.data$technology %in% technologies_lookup) %>%
+    dplyr::filter(dplyr::between(.data$year, start_year, end_year))
+
   report_all_duplicate_kinds(
     data = capacity_factors_power_filtered,
     composite_unique_cols = c("scenario", "scenario_geography", "technology", "year"))
@@ -25,6 +32,10 @@ check_and_filter_data <- function(st_data_list, start_year, end_year, scenarios_
   report_all_duplicate_kinds(
     data = df_price_filtered,
     composite_unique_cols = c("year", "sector", "technology"))
+
+  report_all_duplicate_kinds(
+    data = scenario_data_filtered,
+    composite_unique_cols = c("scenario_geography", "scenario", "ald_sector", "technology", "year"))
 
   return(list(
     capacity_factors_power = capacity_factors_power_filtered,
