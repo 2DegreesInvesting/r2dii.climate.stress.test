db35ff8332cffa268375715ddeb73ef3f95a0238
MirijaHa <mirja@2degrees-investing.org>
2021-11-02 17:12:30 +0100

    no need for var

--- R/read_and_prepare.R
+++ R/read_and_prepare.R
@@ -126,13 +126,12 @@ read_sector_exposures <- function(path) {
 
   sector_exposures <- readr::read_rds(path)
 
-  expected_columns <- c(
-    "investor_name", "portfolio_name", "valid_value_usd", "valid_input",
-    "asset_type", "financial_sector"
-    )
   validate_data_has_expected_cols(
     data = sector_exposures,
-    expected_columns = expected_columns
+    expected_columns = c(
+      "investor_name", "portfolio_name", "valid_value_usd", "valid_input",
+      "asset_type", "financial_sector"
+    )
   )
 
   return(sector_exposures)
