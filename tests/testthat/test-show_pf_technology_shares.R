176543f188d5578690e178c3f3354d37352614dd
MirijaHa <mirja@2degrees-investing.org>
2021-11-03 10:53:57 +0100

    user readr

--- tests/testthat/test-show_pf_technology_shares.R
+++ tests/testthat/test-show_pf_technology_shares.R
@@ -1,6 +1,6 @@
 test_that("portfolio technology share function returns a ggplot object", {
   # NOTE: the test annual profits are entirely made up and inconsistent on purpose
-  test_pf_tech_shares <- readRDS(
+  test_pf_tech_shares <- readr::read_rds(
     testthat::test_path("test_data", "test_pf_tech_share.rds")
   )
 
