833f0d6d7302ebdb71574ecbf5b22fabdb1e1885
jacobvjk <jacob.kastl@gmail.com>
2021-11-04 16:07:09 +0100

    reead financial data from csv

--- R/read_financial_data.R
+++ R/read_financial_data.R
@@ -13,7 +13,18 @@ read_financial_data <- function(path = NULL) {
   valid_input_file_path <- file.exists(file.path(path))
   stopifnot(valid_input_file_path)
 
-  data <- readr::read_rds(path)
+  data <- readr::read_csv(
+    path,
+    col_types = readr::cols_only(
+      company_name = "c",
+      company_id = "d",
+      corporate_bond_ticker = "c",
+      pd = "d",
+      net_profit_margin = "d",
+      debt_equity_ratio = "d",
+      volatility = "d"
+    )
+  )
 
   expected_columns <- c(
     "company_name", "company_id", "corporate_bond_ticker", "pd",
