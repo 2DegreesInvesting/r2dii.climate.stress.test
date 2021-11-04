653ecd6d3f73bb81bdfc910d3ef97c46d52f2b31
jacobvjk <jacob.kastl@gmail.com>
2021-11-04 16:16:25 +0100

    adjust path in function call

--- R/read_and_prepare.R
+++ R/read_and_prepare.R
@@ -57,7 +57,7 @@ read_and_prepare_project_agnostic_data <- function(start_year, end_year, company
     )
 
   financial_data <- read_financial_data(
-    path = file.path(get_st_data_path(), "prewrangled_financial_data_stress_test.rds")
+    path = file.path(get_st_data_path(), "prewrangled_financial_data_stress_test.csv")
   ) %>%
   wrangle_financial_data(asset_type = asset_type)
 
