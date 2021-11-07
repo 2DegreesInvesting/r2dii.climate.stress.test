409a076851c58493775605c94a6186eac646f284
Mauro Lepore <maurolepore@gmail.com>
2021-11-07 12:34:29 -0600

    Refactor

--- tests/testthat/test-run_stress_test.R
+++ tests/testthat/test-run_stress_test.R
@@ -1,12 +1,7 @@
 test_that("works with bonds", {
   skip_if_not(is_registered_dev())
 
-  none <- NA
-  expect_error({
-    # Quietly
-    x <- capture.output(suppressWarnings(
-      run_stress_test("bonds")
-    ))},
-    none
-  )
+  expect_no_error({
+    x <- capture.output(suppressWarnings(run_stress_test("bonds")))
+  })
 })
