7e6df008e7f58a9ab4ac90e7633e26ddc0f57a71
Mauro Lepore <maurolepore@gmail.com>
2021-11-07 12:14:29 -0600

    Remove dead code

--- R/utils-tests.R
+++ R/utils-tests.R
@@ -18,25 +18,6 @@ expect_no_error <- function(object, ...) {
   testthat::expect_error(object, regexp = NA, ...)
 }
 
-# FIXME: Dead code? ----------------------------------------------------------
-
-expect_no_warning <- function(object, ...) {
-  testthat::expect_warning(object, regexp = NA, ...)
-}
-
 outputs_path <- function() {
-  fs::path(Sys.getenv("ST_PROJECT_FOLDER"), "outputs")
-}
-
-dir_empty <- function(dir) {
-  if (fs::dir_exists(dir)) fs::dir_delete(dir)
-  fs::dir_create(dir)
-}
-
-is_empty_dir <- function(dir) {
-  identical(fs::dir_ls(dir), character(0))
-}
-
-ls_results <- function(dir) {
-  fs::dir_ls(dir, regexp = "stress_test_results.*[.]csv$")
+  path(Sys.getenv("ST_PROJECT_FOLDER"), "outputs")
 }
