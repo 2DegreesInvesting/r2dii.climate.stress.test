57e9780af3d9a03467e40157b46e9103bbe4d427
Mauro Lepore <maurolepore@gmail.com>
2021-11-07 11:56:42 -0600

    Remove ::

--- R/st_bonds.R
+++ R/st_bonds.R
@@ -115,7 +115,7 @@ st_write <- function(data, asset_type, destdir = tempdir(), ..., quiet = TRUE) {
   for (i in seq_along(args)) {
     out <- exec(st, !!!args[[i]])
     out <- unnest(out, st_result)
-    readr::write_csv(out, file = path(destdir, glue("{names(args)[[i]]}.csv")))
+    write_csv(out, file = path(destdir, glue("{names(args)[[i]]}.csv")))
   }
 
   invisible(data)
