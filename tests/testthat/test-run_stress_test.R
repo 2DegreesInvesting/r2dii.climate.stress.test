33b8adf015256b7b84de44817e20bbeff21ad7ca
Mauro Lepore <maurolepore@gmail.com>
2021-11-08 10:13:46 -0600

    Polish test

--- tests/testthat/test-run_stress_test.R
+++ tests/testthat/test-run_stress_test.R
@@ -1,4 +1,7 @@
-test_that("works with integer passed to `term`", {
+test_that("works with `term` of type 'integer'", {
+  is_me <- identical(path.expand("~"), "/home/mauro")
+  skip_if_not(is_me)
+
   expect_no_error(
     # Quietly
     x <- suppressWarnings(capture.output(
