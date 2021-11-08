d480c34f818cb0ec6029d6782e6d9ca2610b71a0
MirijaHa <mirja@2degrees-investing.org>
2021-11-08 14:11:20 +0100

    fix order

--- R/run_stress_test.R
+++ R/run_stress_test.R
@@ -103,15 +103,15 @@ run_stress_test <- function(asset_type,
       scenario_geography_filter = scenario_geography_filter
     )
 
-  report_company_drops(
-    data_list = input_data_list,
-    asset_type = asset_type
-  )
-
   if (asset_type == "loans") {
     input_data_list$financial_data <- input_data_list$financial_data %>%
       dplyr::mutate(company_name = stringr::str_to_lower(.data$company_name))
   }
+
+  report_company_drops(
+    data_list = input_data_list,
+    asset_type = asset_type
+  )
 
   check_scenario_availability(
     portfolio = input_data_list$pacta_results,
