e4e531fb0ce820384fa38b9b1e569ceeadd08e9a
MirijaHa <88371355+MirijaHa@users.noreply.github.com>
2021-11-16 13:41:53 +0100

    Bye bye lookups (#218)

--- R/validate.R
+++ R/validate.R
@@ -2,84 +2,28 @@
 #'
 #' Checks that user inputs are of length 1 and within defined ranges.
 #'
-#' @param asset_type String holding asset_type, for allowed value compare
-#'   `asset_types_lookup`.
-#' @param lgd_senior_claims Numeric, holding the loss given default for senior
-#'   claims, for accepted value range check `lgd_senior_claims_range_lookup`.
-#' @param lgd_subordinated_claims Numeric, holding the loss given default for
-#'   subordinated claims, for accepted value range check
-#'   `lgd_subordinated_claims_range_lookup`.
-#' @param terminal_value Numeric. A ratio to determine the share of the
-#'   discounted value used in the terminal value calculation beyond the
-#'   projected time frame. For accepted range compare `terminal_value_range_lookup`.
-#' @param risk_free_rate Numeric that indicates the risk free rate of interest.
-#'   For accepted range compare `risk_free_rate_range_lookup`.
-#' @param discount_rate Numeric, that holds the discount rate of dividends per
-#'   year in the DCF. For accepted range compare `discount_rate_range_lookup`.
-#' @param div_netprofit_prop_coef Numeric. A coefficient that determines how
-#'   strongly the future dividends propagate to the company value. For accepted
-#'   range compare `div_netprofit_prop_coef_range_lookup`.
-#' @param shock_year Numeric, holding year the shock is applied. For accepted
-#'   range compare `shock_year_range_lookup`.
-#' @param term Numeric. A coefficient that determines for which maturity the
-#'   expected loss should be calculated in the credit risk section. For accepted
-#'   range compare `term_range_lookup`.
-#' @param company_exclusion Boolean, indicating if companies provided in dataset
-#'   excluded_companies.csv shall be excluded.
+#' @inheritParams run_stress_test
 #'
 #' @return NULL
 validate_input_values <- function(lgd_senior_claims, lgd_subordinated_claims,
                                   terminal_value, risk_free_rate, discount_rate,
                                   div_netprofit_prop_coef, shock_year, term,
                                   company_exclusion, asset_type) {
-
-  input_args <- list(
-    lgd_senior_claims, lgd_subordinated_claims, terminal_value, risk_free_rate,
-    discount_rate, div_netprofit_prop_coef, shock_year, term, company_exclusion
-  )
+  input_args <- mget(names(formals()), sys.frame(sys.nframe()))
 
   if (any(purrr::map_int(input_args, length) != 1)) {
     stop("Input arguments to stress test run need to be of length 1")
   }
 
-  if (!is.logical(company_exclusion)) {
-    stop("Argmuent company_exclusion must be a boolean.")
-  }
+  c("company_exclusion", "asset_type") %>%
+    purrr::walk(validate_value_in_values, args_list = input_args)
 
-  if (!dplyr::between(lgd_senior_claims, min(lgd_senior_claims_range_lookup), max(lgd_senior_claims_range_lookup))) {
-    stop("Argument lgd_senior_claims is outside accepted range.")
-  }
-
-  if (!dplyr::between(lgd_subordinated_claims, min(lgd_subordinated_claims_range_lookup), max(lgd_subordinated_claims_range_lookup))) {
-    stop("Argument lgd_subordinated_claims is outside accepted range.")
-  }
-
-  if (!dplyr::between(terminal_value, min(terminal_value_range_lookup), max(terminal_value_range_lookup))) {
-    stop("Argument terminal_value is outside accepted range.")
-  }
-
-  if (!dplyr::between(risk_free_rate, min(risk_free_rate_range_lookup), max(risk_free_rate_range_lookup))) {
-    stop("Argument risk_free_rate is outside accepted range.")
-  }
-
-  if (!dplyr::between(discount_rate, min(discount_rate_range_lookup), max(discount_rate_range_lookup))) {
-    stop("Argument discount_rate is outside accepted range.")
-  }
-
-  if (!dplyr::between(div_netprofit_prop_coef, min(div_netprofit_prop_coef_range_lookup), max(div_netprofit_prop_coef_range_lookup))) {
-    stop("Argument div_netprofit_prop_coef is outside accepted range.")
-  }
-
-  if (!dplyr::between(shock_year, min(shock_year_range_lookup), max(shock_year_range_lookup))) {
-    stop("Argument shock_year is outside accepted range.")
-  }
+  c("lgd_senior_claims", "lgd_subordinated_claims", "terminal_value", "risk_free_rate",
+    "discount_rate", "div_netprofit_prop_coef", "shock_year", "term") %>%
+    purrr::walk(validate_value_in_range, args_list = input_args)
 
   if (!shock_year %% 1 == 0) {
     stop("Argmuent shock_year must be a whole number")
-  }
-
-  if (!dplyr::between(term, min(term_range_lookup), max(term_range_lookup))) {
-    stop("Argument term is outside accepted range.")
   }
 
   # ADO 1943 - Once we decide to add a separate Merton calculation on the average
@@ -87,8 +31,101 @@ validate_input_values <- function(lgd_senior_claims, lgd_subordinated_claims,
   if (!term %% 1 == 0) {
     stop("Argmemnt term must be a whole number")
   }
+}
 
-  if (!asset_type %in% asset_types_lookup) {
-    stop("Invalid value for argument asset_type")
+#'  Validate that value in within values
+#'
+#' #' Checks that value of variable `var` is in valid values as defined in
+#' r2dii.climate_stress_test::stress_test_arguments.
+#'
+#' @param var String holding name of variable.
+#' @param args_list Named list holding arguments of parent function call and
+#'   their values.
+#'
+#' @return NULL
+validate_value_in_values <- function(var, args_list) {
+  arg_type <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(.data$type)
+
+  allowed_values <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(.data$allowed) %>%
+    strsplit(",") %>%
+    purrr::pluck(1) %>%
+    purrr::map_chr(trimws) # FIXME: configure in stress_test_arguments without whitespace
+
+  arg_val <- get(var, args_list)
+
+  if (arg_type == "logical") {
+    allowed_values <- as.logical(allowed_values)
+    if (!is.logical(arg_val)) {
+      rlang::abort(
+        c(
+          glue::glue("Must provide valid data type for variable {var}."),
+          x = glue::glue("Invalid type: {typeof(arg_val)}."),
+          i = glue::glue("Valid type is: logical.")
+        )
+      )
+    }
   }
+
+  if (!arg_val %in% allowed_values) {
+    allowed_values_collapsed <- paste0(allowed_values, collapse = ", ")
+    rlang::abort(
+      c(
+        glue::glue("Must provide valid input for argument {var}."),
+        x = glue::glue("Invalid input: {arg_val}."),
+        i = glue::glue("Valid values are: {allowed_values_collapsed}.")
+      )
+    )
+  }
+  invisible()
+}
+
+#' Validate that value in within range
+#'
+#' Checks that value of variable `var` is in valid range as defined in
+#' r2dii.climate_stress_test::stress_test_arguments.
+#'
+#' @inheritParams validate_value_in_values
+#'
+#' @return NULL
+validate_value_in_range <- function(var, args_list) {
+  min <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(.data$min) %>%
+    as.numeric()
+
+  stopifnot(length(min) == 1)
+
+  max <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(.data$max) %>%
+    as.numeric()
+
+  stopifnot(length(max) == 1)
+
+  arg_val <- get(var, args_list)
+
+  if (!is.numeric(arg_val)) {
+    rlang::abort(
+      c(
+        glue::glue("Must provide valid data type for variable {var}."),
+        x = glue::glue("Invalid type: {typeof(arg_val)}."),
+        i = glue::glue("Valid type is: numeric.")
+      )
+    )
+  }
+
+  if (!dplyr::between(arg_val, min, max)) {
+    rlang::abort(
+      c(
+        glue::glue("Must provide valid input for argument {var}."),
+        x = glue::glue("Invalid input: {arg_val}."),
+        i = glue::glue("Is your argument in valid range between {min} and {max}?")
+      )
+    )
+  }
+  invisible()
 }
--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -1,4 +1,36 @@
 # validate_input_values ---------------------------------------------------
+test_that("Error is thrown if input values are of incorrect type", {
+  # logical
+  expect_error(validate_input_values(
+    lgd_senior_claims = 0.45,
+    lgd_subordinated_claims = 0.75,
+    terminal_value = 0,
+    risk_free_rate = 0,
+    discount_rate = 0.02,
+    div_netprofit_prop_coef = 1,
+    shock_year = 2030,
+    term = 4,
+    company_exclusion = "TRUE",
+    asset_type = "loans"
+  ), "logical")
+
+  # numeric
+  expect_error(validate_input_values(
+    lgd_senior_claims = 0.45,
+    lgd_subordinated_claims = 0.75,
+    terminal_value = 0,
+    risk_free_rate = 0,
+    discount_rate = 0.02,
+    div_netprofit_prop_coef = "1",
+    shock_year = 2030,
+    term = 4,
+    company_exclusion = TRUE,
+    asset_type = "bonds"
+  ), "numeric")
+
+})
+
+
 test_that("Error is thrown if an input value is out of bounds", {
   expect_error(validate_input_values(
     lgd_senior_claims = 0.45,
@@ -12,6 +44,21 @@ test_that("Error is thrown if an input value is out of bounds", {
     company_exclusion = TRUE,
     asset_type = "loans"
   ), "risk_free_rate")
+})
+
+test_that("Error is thrown if a character input value is out of bounds", {
+  expect_error(validate_input_values(
+    lgd_senior_claims = 0.45,
+    lgd_subordinated_claims = 0.75,
+    terminal_value = 0,
+    risk_free_rate = 1,
+    discount_rate = 0.02,
+    div_netprofit_prop_coef = 1,
+    shock_year = 2030,
+    term = 4,
+    company_exclusion = TRUE,
+    asset_type = "derivates"
+  ), "asset_type")
 })
 
 test_that("Error is thrown if term is not an integer", {
@@ -34,7 +81,7 @@ test_that("No error is thrown if an input value equals a bound", {
     lgd_senior_claims = 0.45,
     lgd_subordinated_claims = 0.75,
     terminal_value = 0,
-    risk_free_rate = min(risk_free_rate_range_lookup),
+    risk_free_rate = 0,
     discount_rate = 0.02,
     div_netprofit_prop_coef = 1,
     shock_year = 2030,
@@ -51,7 +98,7 @@ test_that("Error is thrown if an input value is of length other than 1", {
     lgd_senior_claims = 0.45,
     lgd_subordinated_claims = 0.75,
     terminal_value = 0,
-    risk_free_rate = min(risk_free_rate_range_lookup),
+    risk_free_rate = 0,
     discount_rate = NULL,
     div_netprofit_prop_coef = 1,
     shock_year = 2030,
@@ -65,7 +112,7 @@ test_that("Error is thrown if an input value is of length other than 1", {
     lgd_senior_claims = 0.45,
     lgd_subordinated_claims = 0.75,
     terminal_value = c(0, 1),
-    risk_free_rate = min(risk_free_rate_range_lookup),
+    risk_free_rate = 0,
     discount_rate = 0.02,
     div_netprofit_prop_coef = 1,
     shock_year = 2030,
