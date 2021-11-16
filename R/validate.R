54a7c1d2295c4bd4c12c8ab59be8590ccca30628
MirijaHa <mirja@2degrees-investing.org>
2021-11-16 11:10:25 +0100

    check in range

--- R/validate.R
+++ R/validate.R
@@ -9,7 +9,6 @@ validate_input_values <- function(lgd_senior_claims, lgd_subordinated_claims,
                                   terminal_value, risk_free_rate, discount_rate,
                                   div_netprofit_prop_coef, shock_year, term,
                                   company_exclusion, asset_type) {
-
   input_args <- mget(names(formals()), sys.frame(sys.nframe()))
 
   if (any(purrr::map_int(input_args, length) != 1)) {
@@ -17,44 +16,14 @@ validate_input_values <- function(lgd_senior_claims, lgd_subordinated_claims,
   }
 
   c("company_exclusion", "asset_type") %>%
-    purrr::walk(validate_value_in_values, named_list = input_args)
+    purrr::walk(validate_value_in_values, args_list = input_args)
 
-  if (!dplyr::between(lgd_senior_claims,
-                      stress_test_arguments %>% dplyr::filter(name == "lgd_senior_claims") %>% dplyr::pull(min),
-                      stress_test_arguments %>% dplyr::filter(name == "lgd_senior_claims") %>% dplyr::pull(max))) {
-    stop("Argument lgd_senior_claims is outside accepted range.")
-  }
-
-  if (!dplyr::between(lgd_subordinated_claims, min(lgd_subordinated_claims_range_lookup), max(lgd_subordinated_claims_range_lookup))) {
-    stop("Argument lgd_subordinated_claims is outside accepted range.")
-  }
-
-  if (!dplyr::between(terminal_value, min(terminal_value_range_lookup), max(terminal_value_range_lookup))) {
-    stop("Argument terminal_value is outside accepted range.")
-  }
-
-  if (!dplyr::between(risk_free_rate, min(risk_free_rate_range_lookup), max(risk_free_rate_range_lookup))) {
-    stop("Argument risk_free_rate is outside accepted range.")
-  }
-
-  if (!dplyr::between(discount_rate, min(discount_rate_range_lookup), max(discount_rate_range_lookup))) {
-    stop("Argument discount_rate is outside accepted range.")
-  }
-
-  if (!dplyr::between(div_netprofit_prop_coef, min(div_netprofit_prop_coef_range_lookup), max(div_netprofit_prop_coef_range_lookup))) {
-    stop("Argument div_netprofit_prop_coef is outside accepted range.")
-  }
-
-  if (!dplyr::between(shock_year, min(shock_year_range_lookup), max(shock_year_range_lookup))) {
-    stop("Argument shock_year is outside accepted range.")
-  }
+  c("lgd_senior_claims", "lgd_subordinated_claims", "terminal_value", "risk_free_rate",
+    "discount_rate", "div_netprofit_prop_coef", "shock_year", "term") %>%
+    purrr::walk(validate_value_in_range, args_list = input_args)
 
   if (!shock_year %% 1 == 0) {
     stop("Argmuent shock_year must be a whole number")
-  }
-
-  if (!dplyr::between(term, min(term_range_lookup), max(term_range_lookup))) {
-    stop("Argument term is outside accepted range.")
   }
 
   # ADO 1943 - Once we decide to add a separate Merton calculation on the average
@@ -62,11 +31,9 @@ validate_input_values <- function(lgd_senior_claims, lgd_subordinated_claims,
   if (!term %% 1 == 0) {
     stop("Argmemnt term must be a whole number")
   }
-
 }
 
-validate_value_in_values <- function(var, named_list) {
-
+validate_value_in_values <- function(var, args_list) {
   arg_type <- stress_test_arguments %>%
     dplyr::filter(.data$name == .env$var) %>%
     dplyr::pull(type)
@@ -82,7 +49,7 @@ validate_value_in_values <- function(var, named_list) {
     allowed_values <- as.logical(allowed_values)
   }
 
-  arg_val <- get(var, named_list)
+  arg_val <- get(var, args_list)
   if (!arg_val %in% allowed_values) {
     allowed_values_collapsed <- paste0(allowed_values, collapse = ", ")
     rlang::abort(
@@ -93,5 +60,32 @@ validate_value_in_values <- function(var, named_list) {
       )
     )
   }
+}
 
+validate_value_in_range <- function(var, args_list) {
+  min <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(min) %>%
+    as.numeric()
+
+  stopifnot(length(min) == 1)
+
+  max <- stress_test_arguments %>%
+    dplyr::filter(.data$name == .env$var) %>%
+    dplyr::pull(max) %>%
+    as.numeric()
+
+  stopifnot(length(max) == 1)
+
+  arg_val <- get(var, args_list)
+
+  if (!dplyr::between(arg_val, min, max)) {
+    rlang::abort(
+      c(
+        glue::glue("Must provide valid input for argument {var}."),
+        x = glue::glue("Invalid input: {arg_val}."),
+        i = glue::glue("Is your argument in valid range between {min} and {max}?")
+      )
+    )
+  }
 }
