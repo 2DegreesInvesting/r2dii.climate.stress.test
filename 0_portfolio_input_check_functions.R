08e6b68e23951419b18d7ec8bc8b492b2064f079
MirijaHa <mirja@2degrees-investing.org>
2021-11-22 15:13:56 +0100

    rm data_check

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -194,19 +194,3 @@ classify_all_funds <- function(fin_data){
   fin_data
 }
 
-### Portfolio Check Functions
-
-# Checks whether a variable is a dataframe. Considers also logicals and null values.
-data_check <- function(df) {
-  if (is.data.frame(df)) {
-    if (nrow(df) > 0) {
-      check <- TRUE
-    } else {
-      check <- FALSE
-    }
-  } else {
-    check <- FALSE
-  }
-
-  return(check)
-}
