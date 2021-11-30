a137efe3c78fbb8ab293d43beefedee7ff69ac1b
MirijaHa <mirja@2degrees-investing.org>
2021-11-30 16:11:17 +0100

    add tests

--- tests/testthat/test-wrangle_and_check.R
+++ tests/testthat/test-wrangle_and_check.R
@@ -11,3 +11,38 @@ test_that("function detects values out of range", {
     asset_type = "equity"
   ), "pd detected")
 })
+
+
+# select_sector_scenario_combinations -------------------------------------
+test_that("Error is thrown if scenarios are missing", {
+  expect_error(select_sector_scenario_combinations(pacta_results = tibble::tibble(
+    sector = c("Automotive", "Automotive", "Power", "Power"),
+    scenario = c("ETP2017_SDS", "ETP2017_ABC", "WEO2019_NPS", "WEO2019_SDS")
+  )), "missing")
+})
+
+test_that("Only required scenarios are kept", {
+  pacta_results <- tibble::tibble(
+    sector = c("Automotive", "Automotive", "Automotive"),
+    scenario = c("ETP2017_SDS", "ETP2017_NPS", "WEO2019_ABC")
+  )
+
+  selected_pacta_results <- select_sector_scenario_combinations(pacta_results = pacta_results)
+
+  expect_equal(
+    pacta_results %>%
+      dplyr::filter(scenario != "WEO2019_ABC"),
+    selected_pacta_results
+  )
+})
+
+test_that("Input is returned unaltered if only required scenarios are provided for sectors", {
+  pacta_results <- tibble::tibble(
+    sector = c("Automotive", "Automotive", "Power", "Power"),
+    scenario = c("ETP2017_SDS", "ETP2017_NPS", "WEO2019_NPS", "WEO2019_SDS")
+  )
+
+  selected_pacta_results <- select_sector_scenario_combinations(pacta_results = pacta_results)
+
+  expect_equal(pacta_results, selected_pacta_results)
+})
