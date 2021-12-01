8a95cfdac6a384c0ee506a0de5be6d13ad3fc270
Mauro Lepore <maurolepore@gmail.com>
2021-12-01 08:58:30 -0600

    Style data-raw/ manually

--- data-raw/stress_test_arguments.R
+++ data-raw/stress_test_arguments.R
@@ -4,7 +4,7 @@ stress_test_arguments <- tibble::tribble(
                "asset_type", "character",       NA, "equity, bonds, loans",         NA,         NA,
         "lgd_senior_claims",    "double",   "0.45",                     NA,      "0.3",      "0.6",
   "lgd_subordinated_claims",    "double",   "0.75",                     NA,      "0.6",      "0.9",
-#           "terminal_value",    "double",      "0",                     NA,        "0",      "0.1",
+   #       "terminal_value",    "double",      "0",                     NA,        "0",      "0.1",
            "risk_free_rate",    "double",   "0.02",                     NA,        "0",     "0.05",
             "discount_rate",    "double",   "0.02",                     NA,    "-0.01",     "0.05",
   "div_netprofit_prop_coef",    "double",      "1",                     NA,      "0.8",        "1",
