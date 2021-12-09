38ec69c1b4ddedb5cbfef7c8581b92cb2b7bb92a
MirijaHa <mirja@2degrees-investing.org>
2021-12-09 12:05:17 +0100

    accidental delete

--- tests/testthat/test-run_stress_test.R
+++ tests/testthat/test-run_stress_test.R
@@ -20,6 +20,9 @@ test_that("with bonds, with iteration, using default settings output is unchange
   ))
 
   expect_snapshot(lapply(results, as.data.frame))
+
+  have_suffix_arg <- purrr::map_lgl(results, ~ rlang::has_name(.x, "term_arg"))
+  expect_true(all(have_suffix_arg))
 })
 
 test_that("with loans, without iteration, using minimum values of input
