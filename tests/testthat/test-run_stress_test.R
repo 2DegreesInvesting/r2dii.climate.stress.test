b1e6407462f9c98e87fefd5611f460a87460e727
MirijaHa <mirja@2degrees-investing.org>
2021-12-09 08:50:23 +0100

    add iterations

--- tests/testthat/test-run_stress_test.R
+++ tests/testthat/test-run_stress_test.R
@@ -23,7 +23,7 @@ test_that("with bonds, with iteration, using default settings output is unchange
   expect_snapshot(lapply(results, as.data.frame))
 })
 
-test_that("with loans, without iteration, using minimal values of input
+test_that("with loans, without iteration, using minimum values of input
           arguments output is unchanged", {
   skip_if_not(opt_in_snapshots())
   skip_on_ci()
@@ -40,7 +40,14 @@ test_that("with loans, without iteration, using minimal values of input
       input_path_project_agnostic = in_agnostic,
       input_path_project_specific = in_specific,
       output_path = out,
-      term = 1,
+      lgd_senior_claims = as.numeric(dplyr::filter(stress_test_arguments, name == "lgd_senior_claims")$min),
+      lgd_subordinated_claims = as.numeric(dplyr::filter(stress_test_arguments, name == "lgd_subordinated_claims")$min),
+      risk_free_rate = as.numeric(dplyr::filter(stress_test_arguments, name == "risk_free_rate")$min),
+      discount_rate = as.numeric(dplyr::filter(stress_test_arguments, name == "discount_rate")$min),
+      div_netprofit_prop_coef = as.numeric(dplyr::filter(stress_test_arguments, name == "div_netprofit_prop_coef")$min),
+      shock_year = as.numeric(dplyr::filter(stress_test_arguments, name == "shock_year")$min),
+      term = as.integer(dplyr::filter(stress_test_arguments, name == "term")$min),
+      company_exclusion = FALSE,
       return_results = TRUE
     )
   ))
@@ -48,7 +55,7 @@ test_that("with loans, without iteration, using minimal values of input
   expect_snapshot(lapply(results, as.data.frame))
 })
 
-test_that("with equity, without iteration, using minimal values of input
+test_that("with equity, without iteration, using maximum values of input
           arguments output is unchanged", {
   skip_if_not(opt_in_snapshots())
   skip_on_ci()
@@ -65,7 +72,14 @@ test_that("with equity, without iteration, using minimal values of input
       input_path_project_agnostic = in_agnostic,
       input_path_project_specific = in_specific,
       output_path = out,
-      term = 1:2,
+      lgd_senior_claims = as.numeric(dplyr::filter(stress_test_arguments, name == "lgd_senior_claims")$max),
+      lgd_subordinated_claims = as.numeric(dplyr::filter(stress_test_arguments, name == "lgd_subordinated_claims")$max),
+      risk_free_rate = as.numeric(dplyr::filter(stress_test_arguments, name == "risk_free_rate")$max),
+      discount_rate = as.numeric(dplyr::filter(stress_test_arguments, name == "discount_rate")$max),
+      div_netprofit_prop_coef = as.numeric(dplyr::filter(stress_test_arguments, name == "div_netprofit_prop_coef")$max),
+      shock_year = as.numeric(dplyr::filter(stress_test_arguments, name == "shock_year")$max),
+      term = as.integer(dplyr::filter(stress_test_arguments, name == "term")$min),
+      company_exclusion = TRUE,
       return_results = TRUE
     )
   ))
