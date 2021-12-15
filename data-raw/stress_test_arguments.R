0fb2d3a37bdb40e45e0ce54f474edac373a7a8f2
jacobvjk <60064070+jacobvjk@users.noreply.github.com>
2021-12-15 12:48:54 +0100

    new deault and lims discount rate (#271)

--- data-raw/stress_test_arguments.R
+++ data-raw/stress_test_arguments.R
@@ -6,7 +6,7 @@ stress_test_arguments <- tibble::tribble(
   "lgd_subordinated_claims",    "double",   "0.75",                     NA,      "0.6",      "0.9",
 #           "terminal_value",    "double",      "0",                     NA,        "0",      "0.1",
            "risk_free_rate",    "double",   "0.02",                     NA,        "0",     "0.05",
-            "discount_rate",    "double",   "0.02",                     NA,    "-0.01",     "0.05",
+            "discount_rate",    "double",   "0.07",                     NA,    "-0.01",      "0.1",
   "div_netprofit_prop_coef",    "double",      "1",                     NA,      "0.8",        "1",
                "shock_year",    "double",   "2030",                     NA,     "2025",     "2035",
                      "term",    "double",      "2",                     NA,        "1",       "10",
