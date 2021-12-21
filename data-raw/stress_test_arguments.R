0a8d201332f7de02bd74c58e69d26ee6f01c4786
MirijaHa <mirja@2degrees-investing.org>
2021-12-21 12:54:13 +0100

    correct data

--- data-raw/stress_test_arguments.R
+++ data-raw/stress_test_arguments.R
@@ -4,14 +4,14 @@ stress_test_arguments <- tibble::tribble(
                "asset_type", "character",       NA, "equity, bonds, loans",         NA,         NA,
         "lgd_senior_claims",    "double",   "0.45",                     NA,      "0.3",      "0.6",
   "lgd_subordinated_claims",    "double",   "0.75",                     NA,      "0.6",      "0.9",
-#           "terminal_value",    "double",      "0",                     NA,        "0",      "0.1",
+#          "terminal_value",    "double",      "0",                     NA,        "0",      "0.1",
            "risk_free_rate",    "double",   "0.02",                     NA,        "0",     "0.05",
             "discount_rate",    "double",   "0.07",                     NA,    "-0.01",      "0.1",
   "div_netprofit_prop_coef",    "double",      "1",                     NA,      "0.8",        "1",
                "shock_year",    "double",   "2030",                     NA,     "2025",     "2035",
                      "term",    "double",      "2",                     NA,        "1",        "5",
         "company_exclusion",   "logical",   "TRUE",          "TRUE, FALSE",         NA,         NA,
-         "use_company_term",   "logical",   "FALSE",         "TRUE, FALSE",         NA,         NA
+        "use_company_terms",   "logical",   "FALSE",         "TRUE, FALSE",         NA,         NA
 )
 # styler: on
 
