bc9f279d27b4fa025b216bb5f5fbb5f1d243438a
MirijaHa <mirja@2degrees-investing.org>
2021-12-22 11:52:33 +0100

    add drop report

--- R/add_terms.R
+++ R/add_terms.R
@@ -19,4 +19,24 @@ add_terms <- function(pacta_results, company_terms, fallback_term) {
     return(pacta_results)
   }
 
+  # keeping track of rows here to alert user if rows were dropped during merge
+  # as this would indicate incorrect company_terms data
+  companies_before_merge <- unique(pacta_results$company_name)
+
+  merged_results <- pacta_results %>%
+    dplyr::inner_join(company_terms, by = "company_name")
+
+  companies_after_merge <- unique(pacta_results$company_name)
+
+  if (length(companies_before_merge) > length(companies_after_merge)) {
+
+    missing_companies <- paste0(setdiff(companies_before_merge, companies_after_merge), collapse = ", ")
+    rlang::abort(c(
+      "All companies considered in this analysis must be provided in company_terms.",
+      x = glue::glue("Missing companies: {missing_companies}."),
+      i = "Please check input data."
+    ))
+  }
+
+
 }
