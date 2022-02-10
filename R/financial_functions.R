521d93452ad4634d7be05c98981aa8d47c3df4e8
jacobvjk <jacob.kastl@gmail.com>
2022-02-10 18:57:11 +0100

    ensure only profits on the additional production of laggards are affected

--- R/financial_functions.R
+++ R/financial_functions.R
@@ -1,12 +1,12 @@
 #' Calculates annual net profits on the company-technology level for the
 #' baseline and late and sudden scenarios. Climate laggards which need to build
 #' out their production in increasing technologies to compensate for their
-#' missed targets, are "punished" by applying their proximity to target as an
-#' adjustment factor on the net profit margin. This ensures that late build out
-#' will not simply translate into increased profits. Additionally, we adjust by
-#' the market share of the company on the given technology. This accounts for
-#' the fact that companies of different size will have different degrees of
-#' difficulty catching up.
+#' missed targets, are "punished" by adjusting the net profit margin on their
+#' additional build out based on their proximity to target and market share within
+#' the given technology. This ensures that late build out will not
+#' proportionally translate into increased profits. The adjustment for the
+#' market share of the company accounts for the fact that companies of different
+#' size will face different degrees of difficulty catching up.
 #'
 #' @param data A data frame containing the production forecasts of companies
 #'   under baseline and late and sudden, market prices/costs, company net profit
@@ -15,11 +15,13 @@
 calculate_net_profits <- function(data) {
   data <- data %>%
     dplyr::mutate(
+      production_compensation = .data$late_sudden - .data$baseline,
       net_profits_baseline = .data$baseline * .data$Baseline_price * .data$net_profit_margin,
       net_profits_ls = dplyr::if_else(
         .data$direction == "declining",
         .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin,
-        .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin * .data$proximity_to_target
+        .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin -
+          .data$production_compensation * .data$late_sudden_price * .data$net_profit_margin * (1 - .data$proximity_to_target)
         # TODO: should the market size penalty only be applied to laggards?
       )
     )
