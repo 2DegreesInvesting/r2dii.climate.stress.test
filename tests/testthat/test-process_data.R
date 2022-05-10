b1945dd32528645f7af1543ad7368795fbda32b8
jacobvjk <jacob.kastl@gmail.com>
2022-05-10 14:43:08 +0200

    add test for removal of high carbon tech with 0 prod over full time frame

--- tests/testthat/test-process_data.R
+++ tests/testthat/test-process_data.R
@@ -109,3 +109,82 @@ test_that("company with zero production value in the start year across all given
 
   unlink(test_log_path)
 })
+
+test_that("company-tech combination with 0 production in a low carbon technology
+          over entire timeframe is not removed", {
+  test_data <- tibble::tribble(
+    ~year, ~ald_sector, ~technology, ~scenario, ~company_name, ~plan_tech_prod, ~plan_carsten,
+    2020, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+    2021, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+    2022, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+    2023, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+    2024, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+    2025, "Power", "NuclearCap", "scenario_a", "company_x", 0, 0.01,
+  )
+  test_start_year <- 2020
+  test_time_horizon <- 5
+  test_log_path <- file.path(tempdir(), "log.txt")
+
+  not_removed <- test_data %>%
+    remove_high_carbon_tech_with_missing_production(
+      start_year = test_start_year,
+      time_horizon = test_time_horizon,
+      log_path = test_log_path
+    )
+  testthat::expect_equal(nrow(not_removed), nrow(test_data))
+
+  unlink(test_log_path)
+})
+
+test_that("company-tech combination with positive production in a high carbon
+          technology over parts of the timeframe is not removed", {
+  test_data <- tibble::tribble(
+    ~year, ~ald_sector, ~technology, ~scenario, ~company_name, ~plan_tech_prod, ~plan_carsten,
+    2020, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2021, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2022, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2023, "Power", "OilCap", "scenario_a", "company_x", 1, 0.01,
+    2024, "Power", "OilCap", "scenario_a", "company_x", 1, 0.01,
+    2025, "Power", "OilCap", "scenario_a", "company_x", 1, 0.01,
+  )
+  test_start_year <- 2020
+  test_time_horizon <- 5
+  test_log_path <- file.path(tempdir(), "log.txt")
+
+  not_removed <- test_data %>%
+    remove_high_carbon_tech_with_missing_production(
+      start_year = test_start_year,
+      time_horizon = test_time_horizon,
+      log_path = test_log_path
+    )
+  testthat::expect_equal(nrow(not_removed), nrow(test_data))
+
+  unlink(test_log_path)
+})
+
+test_that("company-tech combination with 0 production in a high carbon technology
+          over entire timeframe is removed", {
+  test_data <- tibble::tribble(
+    ~year, ~ald_sector, ~technology, ~scenario, ~company_name, ~plan_tech_prod, ~plan_carsten,
+    2020, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2021, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2022, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2023, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2024, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+    2025, "Power", "OilCap", "scenario_a", "company_x", 0, 0.01,
+  )
+
+  test_start_year <- 2020
+  test_time_horizon <- 5
+  test_log_path <- file.path(tempdir(), "log.txt")
+
+  removed <- test_data %>%
+    remove_high_carbon_tech_with_missing_production(
+      start_year = test_start_year,
+      time_horizon = test_time_horizon,
+      log_path = test_log_path
+    )
+  testthat::expect_equal(nrow(removed), 0)
+
+  unlink(test_log_path)
+})
