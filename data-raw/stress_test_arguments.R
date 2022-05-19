5dd46293c7a2ee0a91936161ca96fa75f9e5361b
MirijaHa <mirja@2degrees-investing.org>
2022-05-19 09:04:03 +0200

    fix typo

--- data-raw/stress_test_arguments.R
+++ data-raw/stress_test_arguments.R
@@ -8,7 +8,7 @@ stress_test_arguments <- tibble::tribble(
   "lgd_subordinated_claims",    "double",   "0.75",                     NA,      "0.6",      "0.9",
            "risk_free_rate",    "double",   "0.02",                     NA,        "0",     "0.05",
             "discount_rate",    "double",   "0.07",                     NA,    "0.015",      "0.1",
-              "growth_rate",    "double",   "0.03",                     NA,     "0.01",     "0.99",
+              "growth_rate",    "double",   "0.03",                     NA,     "0.01",    "0.099",
   "div_netprofit_prop_coef",    "double",      "1",                     NA,      "0.8",        "1",
                "shock_year",    "double",   "2030",                     NA,     "2025",     "2035",
             "fallback_term",    "double",      "2",                     NA,        "1",        "5", # NOTE: before changing check cap_terms().
