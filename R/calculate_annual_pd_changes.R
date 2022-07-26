207153d4d8d1ab84f67831c3145fb73e560d0d78
jacobvjk <jacob.kastl@gmail.com>
2022-07-26 17:38:58 +0200

    clarify annual PD calc in lrisk

--- R/calculate_annual_pd_changes.R
+++ R/calculate_annual_pd_changes.R
@@ -59,6 +59,8 @@ calculate_pd_change_annual <- function(data,
     dplyr::group_by(
       .data$scenario_name, .data$scenario_geography, .data$investor_name,
       .data$portfolio_name, .data$id, .data$company_name, .data$ald_sector,
+      # TODO: clarify the appropriate summation for annual PD calculations
+      # .data$debt_equity_ratio, .data$volatility
       .data$year, .data$debt_equity_ratio, .data$volatility
     ) %>%
     dplyr::mutate(
