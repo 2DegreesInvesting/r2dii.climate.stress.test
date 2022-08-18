38f2113aa01746792b3424857f0bf49761374a5b
MirijaHa <mirja@2degrees-investing.org>
2022-08-18 13:35:25 +0200

    rm test

--- tests/testthat/test-wrangle_and_check.R
+++ tests/testthat/test-wrangle_and_check.R
@@ -10,20 +10,3 @@ test_that("function detects values out of range", {
     financial_data = fin_data
   ), "pd detected")
 })
-
-# check_company_terms -----------------------------------------------------
-test_that("Error is thrown if terms < 1 are provided", {
-  expect_error(
-    check_company_terms(
-      tibble::tibble(term = c(0, 2, NA))
-    ), "terms below 1"
-  )
-})
-
-test_that("Error is thrown if terms are provided as decimals", {
-  expect_error(
-    check_company_terms(
-      tibble::tibble(term = c(2, 2.1))
-    ), "whole number"
-  )
-})
