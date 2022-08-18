18700155921f1ba7ec9a3bcd32a061a995d7992b
MirijaHa <mirja@2degrees-investing.org>
2022-08-18 13:38:01 +0200

    reanme

--- R/lookup.R
+++ R/lookup.R
@@ -63,8 +63,8 @@ p4i_p4b_sector_technology_lookup <- tibble::tribble(
 
 # styler: on
 
-# holds names of input arguments to run_trisk that are not model parameters
-setup_vars_lookup <- c("input_path_project_agnostic", "output_path", "iter_var", "return_results")
+# holds names of input arguments to run_*risk that are not model parameters
+setup_vars_lookup <- c("input_path", "output_path", "iter_var", "return_results")
 
 high_carbon_tech_lookup <- c("ICE", "Coal", "Oil", "Gas", "CoalCap", "GasCap", "OilCap")
 
