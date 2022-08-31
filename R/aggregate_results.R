f554084e03e7a7f10abdf89e1020a8d48c5dd66a
frmfisc <110397916+frmfisc@users.noreply.github.com>
2022-08-31 16:54:59 +0200

    style

--- R/aggregate_results.R
+++ R/aggregate_results.R
@@ -24,14 +24,15 @@ aggregate_results <- function(results_list, sensitivity_analysis_vars, iter_var,
 
   # Aggregate Crispy Results -----
 
-  merge_by_cols <- c("investor_name", "portfolio_name", "scenario_name", "scenario_geography",
-                     "company_name", "ald_sector", "asset_type_arg", "baseline_scenario_arg",
-                     "shock_scenario_arg", "lgd_arg", "risk_free_rate_arg", "discount_rate_arg",
-                     "growth_rate_arg", "div_netprofit_prop_coef_arg", "shock_year_arg",
-                     "fallback_term_arg", "use_company_terms_arg")
+  merge_by_cols <- c(
+    "investor_name", "portfolio_name", "scenario_name", "scenario_geography",
+    "company_name", "ald_sector", "asset_type_arg", "baseline_scenario_arg",
+    "shock_scenario_arg", "lgd_arg", "risk_free_rate_arg", "discount_rate_arg",
+    "growth_rate_arg", "div_netprofit_prop_coef_arg", "shock_year_arg",
+    "fallback_term_arg", "use_company_terms_arg"
+  )
 
   if (risk_type == "lrisk") {
-
     lrisk_additional <- c("scc_arg", "settlement_factor_arg", "exp_share_damages_paid_arg")
     merge_by_cols <- append(merge_by_cols, lrisk_additional)
   }
@@ -74,17 +75,17 @@ aggregate_results <- function(results_list, sensitivity_analysis_vars, iter_var,
       dplyr::rename(
         scc = .data$scc_arg,
         settlement_factor = .data$settlement_factor_arg,
-        exp_share_damages_paid = .data$exp_share_damages_paid_arg)
+        exp_share_damages_paid = .data$exp_share_damages_paid_arg
+      )
   }
 
   crispy_output <- crispy_output %>%
-      dplyr::mutate(
-        net_present_value_difference = .data$net_present_value_shock - .data$net_present_value_baseline,
-        pd_difference = .data$pd_shock - .data$pd_baseline
-      )
+    dplyr::mutate(
+      net_present_value_difference = .data$net_present_value_shock - .data$net_present_value_baseline,
+      pd_difference = .data$pd_shock - .data$pd_baseline
+    )
 
   if (risk_type == "lrisk") {
-
     crispy_output <- crispy_output %>%
       dplyr::select(
         .data$company_name, .data$sector, .data$business_unit,
@@ -94,21 +95,19 @@ aggregate_results <- function(results_list, sensitivity_analysis_vars, iter_var,
         .data$growth_rate, .data$scc, .data$settlement_factor, .data$exp_share_damages_paid,
         .data$shock_year, .data$net_present_value_baseline,
         .data$net_present_value_shock, .data$net_present_value_difference,
-        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference)
-  }
-  else {
-
+        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference
+      )
+  } else {
     crispy_output <- crispy_output %>%
       dplyr::select(
         .data$company_name, .data$sector, .data$business_unit,
         .data$roll_up_type, .data$scenario_geography, .data$calculation_type,
         .data$baseline_scenario, .data$shock_scenario, .data$lgd,
         .data$risk_free_rate, .data$discount_rate, .data$dividend_rate,
-        .data$growth_rate,.data$shock_year, .data$net_present_value_baseline,
+        .data$growth_rate, .data$shock_year, .data$net_present_value_baseline,
         .data$net_present_value_shock, .data$net_present_value_difference,
-        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference)
-
-
+        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference
+      )
   }
 
 
