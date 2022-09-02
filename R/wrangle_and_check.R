e9925f0fff741b1c27666795670bb4422ee9d555
MirijaHa <mirja@2degrees-investing.org>
2022-09-02 14:03:43 +0200

    logic for wrangling

--- R/wrangle_and_check.R
+++ R/wrangle_and_check.R
@@ -182,16 +182,22 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars) {
     )
 
   # Crispy Results -----
+  merge_by_cols <- c(
+    "scenario_name", "scenario_geography", "company_name", "ald_sector",
+    "baseline_scenario_arg", "shock_scenario_arg", "lgd_arg",
+    "risk_free_rate_arg", "discount_rate_arg", "growth_rate_arg",
+    "div_netprofit_prop_coef_arg", "shock_year_arg"
+  )
+
+  if (risk_type == "lrisk") {
+    lrisk_additional <- c("scc_arg", "settlement_factor_arg", "exp_share_damages_paid_arg")
+    merge_by_cols <- c(merge_by_cols, lrisk_additional)
+  }
 
   crispy_output <- results_list$company_technology_npv %>%
     dplyr::inner_join(
       results_list$company_pd_changes_overall,
-      by = c(
-        "scenario_name", "scenario_geography", "company_name", "ald_sector",
-        "baseline_scenario_arg", "shock_scenario_arg", "lgd_arg",
-        "risk_free_rate_arg", "discount_rate_arg", "growth_rate_arg",
-        "div_netprofit_prop_coef_arg", "shock_year_arg"
-      )
+      by = merge_by_cols
     )
 
   crispy_output <- crispy_output %>%
@@ -210,21 +216,47 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars) {
       net_present_value_shock = .data$total_disc_npv_ls,
       pd_baseline = .data$PD_baseline,
       pd_shock = .data$PD_late_sudden
-    ) %>%
+    )
+
+  if (risk_type == "lrisk") {
+    crispy_output <- crispy_output %>%
+      dplyr::rename(
+        scc = .data$scc_arg,
+        settlement_factor = .data$settlement_factor_arg,
+        exp_share_damages_paid = .data$exp_share_damages_paid_arg
+      )
+  }
+
+  crispy_output <- crispy_output %>%
     dplyr::mutate(
       net_present_value_difference = .data$net_present_value_shock - .data$net_present_value_baseline,
       pd_difference = .data$pd_shock - .data$pd_baseline
-    ) %>%
-    dplyr::select(
-      .data$company_name, .data$sector, .data$business_unit,
-      .data$roll_up_type, .data$scenario_geography,  .data$baseline_scenario,
-      .data$shock_scenario, .data$lgd, .data$risk_free_rate, .data$discount_rate,
-      .data$dividend_rate, .data$growth_rate, .data$shock_year,
-      .data$net_present_value_baseline, .data$net_present_value_shock,
-      .data$net_present_value_difference, .data$term, .data$pd_baseline,
-      .data$pd_shock, .data$pd_difference
     )
 
+  if (risk_type == "lrisk") {
+    crispy_output <- crispy_output %>%
+      dplyr::select(
+        .data$company_name, .data$sector, .data$business_unit,
+        .data$roll_up_type, .data$scenario_geography,
+        .data$baseline_scenario, .data$shock_scenario, .data$lgd,
+        .data$risk_free_rate, .data$discount_rate, .data$dividend_rate,
+        .data$growth_rate, .data$scc, .data$settlement_factor, .data$exp_share_damages_paid,
+        .data$shock_year, .data$net_present_value_baseline,
+        .data$net_present_value_shock, .data$net_present_value_difference,
+        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference
+      )
+  } else {
+    crispy_output <- crispy_output %>%
+      dplyr::select(
+        .data$company_name, .data$sector, .data$business_unit,
+        .data$roll_up_type, .data$scenario_geography,
+        .data$baseline_scenario, .data$shock_scenario, .data$lgd,
+        .data$risk_free_rate, .data$discount_rate, .data$dividend_rate,
+        .data$growth_rate, .data$shock_year, .data$net_present_value_baseline,
+        .data$net_present_value_shock, .data$net_present_value_difference,
+        .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference
+      )
+  }
   return(list(
     company_trajectories = company_trajectories,
     crispy_output = crispy_output
