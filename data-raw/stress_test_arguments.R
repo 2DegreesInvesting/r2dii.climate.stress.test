1420549e4bf17edc2670b069cf68db0383044fc1
frmfisc <110397916+frmfisc@users.noreply.github.com>
2022-10-19 16:16:03 +0200

    delete 2019

--- data-raw/stress_test_arguments.R
+++ data-raw/stress_test_arguments.R
@@ -2,8 +2,8 @@
 stress_test_arguments <- tibble::tribble(
                       ~name,       ~type, ~default,               ~allowed,       ~min,       ~max,
                "asset_type", "character",       NA, "equity, bonds, loans",         NA,         NA,
-        "baseline_scenario", "character", "WEO2020_SPS", "WEO2019_SPS, WEO2020_SPS, GECO2019_ref",         NA,         NA,
-           "shock_scenario", "character", "WEO2020_SDS", "WEO2019_SDS, WEO2020_SDS, GECO2019_1.5c, GECO2019_2c_m",         NA,         NA,
+        "baseline_scenario", "character", "WEO2020_SPS", "WEO2020_SPS, GECO2019_ref",         NA,         NA,
+           "shock_scenario", "character", "WEO2020_SDS", "WEO2020_SDS, GECO2019_1.5c, GECO2019_2c_m",         NA,         NA,
                       "lgd",    "double",   "0.45",                     NA,      "0.3",      "0.9",
            "risk_free_rate",    "double",   "0.02",                     NA,        "0",     "0.05",
             "discount_rate",    "double",   "0.07",                     NA,    "0.015",      "0.1",
