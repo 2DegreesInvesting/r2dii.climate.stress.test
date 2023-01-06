25bf60549d0828857326baa3f0ce756542900bd7
Franziska Fischer <110397916+frmfisc@users.noreply.github.com>
2023-01-06 14:52:29 +0100

    Add carbon price file (#440)
    * add file path to carbon tax file
    
    * add carbon tax file path to the read in function
    
    * automatic change
    
    * automatic change
    
    * autimatic change
    
    * new file read in carbon data
    
    * new file automatically added

---
+++ R/read_carbon_data.R
@@ -0,0 +1,25 @@
+#' Read in carbon price data from ngfs data
+#'
+#'
+#' @param path A string that points to the location of the file containing the
+#'   carbon price data.
+#'
+#' @family import functions
+read_carbon_data <- function(path = NULL) {
+  path %||% stop("Must provide 'path'")
+
+  data <- validate_file_exists(file.path(path)) %>%
+    readr::read_csv(
+      col_types = readr::cols_only(
+        year = "d",
+        model = "c",
+        scenario = "c",
+        scenario_geography = "c",
+        variable = "c",
+        unit = "c",
+        carbon_tax = "d"
+      )
+    )
+
+  return(data)
+}
