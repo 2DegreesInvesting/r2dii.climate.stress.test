ccf9a5a2c5a64f8dcfb2dc45073e402f17852ffc
frmfisc <110397916+frmfisc@users.noreply.github.com>
2023-01-09 11:19:20 +0100

    adjust test for financial functions

--- tests/testthat/test-financial_functions.R
+++ tests/testthat/test-financial_functions.R
@@ -1,12 +1,28 @@
 test_that("calculate_net_profits penalizes companies for late build out of low
           carbon technologies", {
   input_data <- tibble::tribble(
-    ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target,
-    "leader", 100, 150, 10, 10, 0.1, "increasing", 1,
-    "laggard", 100, 150, 10, 10, 0.1, "increasing", 0.5
+    ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target, ~scenario_geography, ~year, ~emission_factor,
+    "leader", 100, 150, 10, 10, 0.1, "increasing", 1, "Global", 2030, 1,
+    "laggard", 100, 150, 10, 10, 0.1, "increasing", 0.5, "Global", 2030, 1
+
   )
 
-  net_profits <- calculate_net_profits(input_data)
+  carbon_data_test <- tibble::tribble(
+    ~year, ~model, ~scenario, ~scenario_geography, ~variable, ~unit, ~carbon_tax,
+    2030, "MESSAGEix-GLOBIOM 1.0", "Current policies (Hot house world, Rep)", "Global", "Price|Carbon", 10, 10,
+
+  )
+
+
+  test_shock_year <- 2021
+  test_end_year_lookup <- 2040
+
+  net_profits <- calculate_net_profits(input_data,
+                                       carbon_data = carbon_data_test,
+                                       shock_year = test_shock_year,
+                                       end_year = test_end_year_lookup
+
+                                       )
 
   net_profits_baseline_climate_leader <- net_profits %>%
     dplyr::filter(.data$company_name == "leader") %>%
@@ -37,12 +53,25 @@ test_that("calculate_net_profits penalizes companies for late build out of low
 test_that("calculate_net_profits does not apply penalty on lost profits for high
           carbon technologies", {
   input_data <- tibble::tribble(
-    ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target,
-    "leader", 100, 50, 10, 10, 0.1, "declining", 1,
-    "laggard", 100, 50, 10, 10, 0.1, "declining", 0.5
+    ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target, ~scenario_geography, ~year, ~emission_factor,
+    "leader", 100, 50, 10, 10, 0.1, "declining", 1, "Global", 2030, 1,
+    "laggard", 100, 50, 10, 10, 0.1, "declining", 0.5, "Global", 2030, 1
   )
 
-  net_profits <- calculate_net_profits(input_data)
+  carbon_data_test <- tibble::tribble(
+    ~year, ~model, ~scenario, ~scenario_geography, ~variable, ~unit, ~carbon_tax,
+    2030, "MESSAGEix-GLOBIOM 1.0", "Current policies (Hot house world, Rep)", "Global", "Price|Carbon", 10, 10,
+
+  )
+
+
+  test_shock_year <- 2021
+  test_end_year_lookup <- 2040
+
+  net_profits <- calculate_net_profits(input_data,
+                                       carbon_data = carbon_data_test,
+                                       shock_year = test_shock_year,
+                                       end_year = test_end_year_lookup)
 
   net_profits_baseline_climate_leader <- net_profits %>%
     dplyr::filter(.data$company_name == "leader") %>%
