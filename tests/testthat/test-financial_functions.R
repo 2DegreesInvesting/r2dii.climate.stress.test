fff1ab7320a1f8d620a9eb70f60c0d28976ff12b
frmfisc <110397916+frmfisc@users.noreply.github.com>
2023-01-10 11:57:34 +0100

    style

--- tests/testthat/test-financial_functions.R
+++ tests/testthat/test-financial_functions.R
@@ -4,13 +4,11 @@ test_that("calculate_net_profits penalizes companies for late build out of low
     ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target, ~scenario_geography, ~year, ~emission_factor,
     "leader", 100, 150, 10, 10, 0.1, "increasing", 1, "Global", 2030, 1,
     "laggard", 100, 150, 10, 10, 0.1, "increasing", 0.5, "Global", 2030, 1
-
   )
 
   carbon_data_test <- tibble::tribble(
     ~year, ~model, ~scenario, ~variable, ~unit, ~carbon_tax, ~scenario_geography,
     2030, "MESSAGEix-GLOBIOM 1.0", "Current policies (Hot house world, Rep)", "Price|Carbon", 10, 10, "Global"
-
   )
 
 
@@ -18,11 +16,10 @@ test_that("calculate_net_profits penalizes companies for late build out of low
   test_end_year_lookup <- 2040
 
   net_profits <- calculate_net_profits(input_data,
-                                       carbon_data = carbon_data_test,
-                                       shock_year = test_shock_year,
-                                       end_year = test_end_year_lookup
-
-                                       )
+    carbon_data = carbon_data_test,
+    shock_year = test_shock_year,
+    end_year = test_end_year_lookup
+  )
 
   net_profits_baseline_climate_leader <- net_profits %>%
     dplyr::filter(.data$company_name == "leader") %>%
@@ -68,9 +65,10 @@ test_that("calculate_net_profits does not apply penalty on lost profits for high
   test_end_year_lookup <- 2040
 
   net_profits <- calculate_net_profits(input_data,
-                                       carbon_data = carbon_data_test,
-                                       shock_year = test_shock_year,
-                                       end_year = test_end_year_lookup)
+    carbon_data = carbon_data_test,
+    shock_year = test_shock_year,
+    end_year = test_end_year_lookup
+  )
 
   net_profits_baseline_climate_leader <- net_profits %>%
     dplyr::filter(.data$company_name == "leader") %>%
