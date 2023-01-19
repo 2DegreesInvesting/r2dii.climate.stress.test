feaf4657b437224f0d317f0b85641396d97f64cb
Franziska Fischer <110397916+frmfisc@users.noreply.github.com>
2023-01-19 14:38:58 +0100

    No carbon tax break net profits (#444)
    * split up net profits non-carbon tax
    
    * styler
    
    * automatically added
    
    * automoatized add
    
    * automatically added
    
    * automatically added
    
    * specfiy direction
    
    * small adjustments
    
    * function description
    
    * add unit test
    
    * delete browser
    
    * man file
    
    * man file
    
    * styler
    
    * roxygen skeleton plus description update

--- R/financial_functions.R
+++ R/financial_functions.R
@@ -45,22 +45,90 @@ calculate_net_profits <- function(data,
     )
 }
 
+
+#' Calculates annual net profits on the company-technology level for the
+#' baseline and late and sudden scenarios - without a carbon tax being added.
+#'
+#' @param data A data frame containing the production forecasts of companies
+#'   under baseline and late and sudden, market prices/costs, company net profit
+#'   margins, the proximity to target in the production forecast period and an
+#'   indication of the direction of the technology.
+#'
+#' @return Data frame with annual netprofits for all cases without carbon tax
 calculate_net_profits_without_carbon_tax <- function(data) {
-  data <- data %>%
-    dplyr::mutate(
-      production_compensation = .data$late_sudden - .data$baseline,
-      net_profits_baseline = .data$baseline * .data$Baseline_price * .data$net_profit_margin,
-      net_profits_ls = dplyr::if_else(
-        .data$direction == "declining",
-        .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin,
-        .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin -
-          .data$production_compensation * .data$late_sudden_price * .data$net_profit_margin * (1 - .data$proximity_to_target)
-        # TODO: ADO4109 - should the market size penalty only be applied to laggards?
-      )
-    )
+  baseline <- calculate_net_profits_baseline(data)
+  shock_increasing_technologies <- calculate_net_profits_shock_increasing_technologies(data = data %>% dplyr::filter(.data$direction == "increasing"))
+  shock_declining_technologies <- calculate_net_profits_shock_declining_technologies(data = data %>% dplyr::filter(.data$direction == "declining"))
+
+  data <- dplyr::full_join(shock_increasing_technologies, shock_declining_technologies)
+  data <- dplyr::full_join(data, baseline)
+
+  return(data)
 }
 
 
+#' Calculates annual net profits on the company-technology level for the baseline scenario
+#'
+#' @param data A data frame containing the production forecasts of companies
+#'   under baseline and late and sudden, market prices/costs, company net profit
+#'   margins, the proximity to target in the production forecast period and an
+#'   indication of the direction of the technology.
+#'
+#' @return A data frame with net profits for the baseline scenario.
+
+calculate_net_profits_baseline <- function(data) {
+  data <- data %>%
+    dplyr::mutate(net_profits_baseline = .data$baseline * .data$Baseline_price * .data$net_profit_margin)
+
+  return(data)
+}
+
+
+#' Calculates annual net profits on the company-technology level for the shock scenario for declining technologies
+#'
+#' @param data A data frame containing the production forecasts of companies
+#'   under baseline and late and sudden, market prices/costs, company net profit
+#'   margins, the proximity to target in the production forecast period and an
+#'   indication of the direction of the technology.
+#'
+#' @return A data frame with net profits of companies with a declining technology
+
+calculate_net_profits_shock_declining_technologies <- function(data) {
+  data <- data %>%
+    dplyr::mutate(net_profits_ls = .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin)
+
+  return(data)
+}
+
+
+#' Calculates annual net profits on the company-technology level for the shock scenario for increasing technologies.
+#' Climate laggards which need to build out their production in increasing technologies to compensate for their
+#' missed targets, are "punished" by adjusting the net profit margin on their
+#' additional build out based on their proximity to target within the given
+#' technology. Specifically, we measure the ratio of how much of the required
+#' build out or reduction in a technology the company will have done at the end
+#' of the forecast period. If the technology has an increasing target and the
+#' ratio of completion is below one, the net_profit_margin on the additional production build out
+#' is multiplied with the proximity to the target. This approximates the additional capital investment
+#' such a company would have to make in a short time, which leads to added costs. This ensures that late
+#' build out will not proportionally translate into increased profits.
+#'
+#' @param data A data frame containing the production forecasts of companies with increasing under the late and sudden,
+#' market prices/costs, company net profit margins, the proximity to target in the production forecast period and an
+#' indication of the direction of the technology.
+#'
+#' @return  A data frame with net profits of companies with a increasing technology
+
+calculate_net_profits_shock_increasing_technologies <- function(data) {
+  data <- data %>%
+    dplyr::mutate(
+      production_compensation = .data$late_sudden - .data$baseline,
+      net_profits_ls = .data$late_sudden * .data$late_sudden_price * .data$net_profit_margin -
+        .data$production_compensation * .data$late_sudden_price * .data$net_profit_margin * (1 - .data$proximity_to_target)
+    )
+
+  return(data)
+}
 
 #' Calculates discounted net profits based on a dividends discount model
 #'
--- tests/testthat/test-financial_functions.R
+++ tests/testthat/test-financial_functions.R
@@ -95,3 +95,44 @@ test_that("calculate_net_profits does not apply penalty on lost profits for high
     net_profits_late_sudden_climate_laggard
   )
 })
+
+
+test_that("calculate_net_profits penalizes companies for late build out of low
+          carbon technologies", {
+  input_data <- tibble::tribble(
+    ~company_name, ~baseline, ~late_sudden, ~Baseline_price, ~late_sudden_price, ~net_profit_margin, ~direction, ~proximity_to_target, ~year,
+    "leader", 100, 150, 10, 10, 0.1, "increasing", 1, 2030,
+    "laggard", 100, 150, 10, 10, 0.1, "increasing", 0.5, 2030,
+  )
+
+
+  test_shock_year <- 2021
+  test_end_year_lookup <- 2040
+
+  net_profits <- calculate_net_profits_without_carbon_tax(input_data)
+
+  net_profits_baseline_climate_leader <- net_profits %>%
+    dplyr::filter(.data$company_name == "leader") %>%
+    dplyr::pull(.data$net_profits_baseline)
+
+  net_profits_baseline_climate_laggard <- net_profits %>%
+    dplyr::filter(.data$company_name == "laggard") %>%
+    dplyr::pull(.data$net_profits_baseline)
+
+  net_profits_late_sudden_climate_leader <- net_profits %>%
+    dplyr::filter(.data$company_name == "leader") %>%
+    dplyr::pull(.data$net_profits_ls)
+
+  net_profits_late_sudden_climate_laggard <- net_profits %>%
+    dplyr::filter(.data$company_name == "laggard") %>%
+    dplyr::pull(.data$net_profits_ls)
+
+  testthat::expect_equal(
+    net_profits_baseline_climate_leader,
+    net_profits_baseline_climate_laggard
+  )
+  testthat::expect_gt(
+    net_profits_late_sudden_climate_leader,
+    net_profits_late_sudden_climate_laggard
+  )
+})
