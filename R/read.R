aa19843161a3d31081cac44866d0542041f90459
frmfisc <110397916+frmfisc@users.noreply.github.com>
2023-02-07 15:35:53 +0100

    small changes

--- R/read.R
+++ R/read.R
@@ -7,24 +7,30 @@ st_read_agnostic <- function(dir, start_year, sectors, risk_type) {
     capacity_factors_power <- NULL
   }
 
-  if (risk_type == "trisk") {
+
     out <- list(
       capacity_factors_power = capacity_factors_power,
       df_price = read_price_data(price_data_file(dir)),
       scenario_data = read_scenario_data(scenario_data_file(dir, start_year)),
       financial_data = read_financial_data(financial_data_file(dir)),
-      production_data = read_production_data(production_data_file(dir)),
+      production_data = read_production_data(production_data_file(dir)))
+
+     if (risk_type == "trisk") {
       carbon_data = read_carbon_data(carbon_price_data_file(dir))
-    )
-  } else {
-    out <- list(
-      capacity_factors_power = capacity_factors_power,
-      df_price = read_price_data(price_data_file(dir)),
-      scenario_data = read_scenario_data(scenario_data_file(dir, start_year)),
-      financial_data = read_financial_data(financial_data_file(dir)),
-      production_data = read_production_data(production_data_file(dir))
-    )
-  }
+
+      out[[length(out)+1]] <- carbon_data
+      }
+browser()
+
+  # } else {
+  #   out <- list(
+  #     capacity_factors_power = capacity_factors_power,
+  #     df_price = read_price_data(price_data_file(dir)),
+  #     scenario_data = read_scenario_data(scenario_data_file(dir, start_year)),
+  #     financial_data = read_financial_data(financial_data_file(dir)),
+  #     production_data = read_production_data(production_data_file(dir))
+  #   )
+  # }
 
   return(out)
 }
