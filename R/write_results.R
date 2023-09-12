741faf63cd6b0d32222c600985626fb666b89279
frmfisc <110397916+frmfisc@users.noreply.github.com>
2023-09-12 02:03:26 +0200

    write carbon tax in file

--- R/write_results.R
+++ R/write_results.R
@@ -7,19 +7,43 @@
 #' @param output_path String holding path to output dir.
 #' @param shock_scenario String holding shock scenario name.
 #' @param scenario_geography String holding scenario geography name.
+#' @param carbon_price_model String holding carbon price model for trisk.
+#' @param risk_type String holding the risk type.
 #'
 #' @return NULL
-write_stress_test_results <- function(results_list, iter_var, shock_scenario, scenario_geography,
+write_stress_test_results <- function(results_list, iter_var, shock_scenario, scenario_geography, carbon_price_model, risk_type,
                                       output_path) {
+
+  if (risk_type == "trisk") {
   results_list$company_trajectories %>%
     readr::write_csv(file.path(
       output_path,
-      glue::glue("company_trajectories_{iter_var}_{shock_scenario}_{scenario_geography}.csv")
+      glue::glue("company_trajectories_{iter_var}_{shock_scenario}_{scenario_geography}_{carbon_price_model}.csv")
     ))
+  }
 
+  if (risk_type == "lrisk") {
+    results_list$company_trajectories %>%
+      readr::write_csv(file.path(
+        output_path,
+        glue::glue("company_trajectories_{iter_var}_{shock_scenario}_{scenario_geography}.csv")
+      ))
+  }
+
+
+  if (risk_type == "trisk") {
   results_list$crispy_output %>%
     readr::write_csv(file.path(
       output_path,
-      glue::glue("crispy_output_{iter_var}_{shock_scenario}_{scenario_geography}.csv")
+      glue::glue("crispy_output_{iter_var}_{shock_scenario}_{scenario_geography}_{carbon_price_model}.csv")
     ))
+  }
+
+  if (risk_type == "lrisk") {
+    results_list$crispy_output %>%
+      readr::write_csv(file.path(
+        output_path,
+        glue::glue("crispy_output_{iter_var}_{shock_scenario}_{scenario_geography}.csv")
+      ))
+  }
 }
