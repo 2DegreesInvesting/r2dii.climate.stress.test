ae1eed889a8d5f39f44b8901eb489628ffe27be0
bertrand <bertrand.gallice.pro@gmail.com>
2024-01-16 14:20:15 +0100

    add all input params as columns in output files

--- R/wrangle_and_check.R
+++ R/wrangle_and_check.R
@@ -184,23 +184,19 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars, risk_type) 
 
   # Crispy Results -----
   merge_by_cols <- c(
-    "scenario_name", "scenario_geography", "company_name", "ald_sector",
-    "baseline_scenario_arg", "shock_scenario_arg", "lgd_arg",
-    "risk_free_rate_arg", "discount_rate_arg", "growth_rate_arg",
-    "div_netprofit_prop_coef_arg", "shock_year_arg", "start_year_arg",
-    "scenario_geography_arg"
+    "company_id", "ald_sector", "scenario_name", "scenario_geography", sensitivity_analysis_vars
   )
-
-  if (risk_type == "lrisk") {
-    lrisk_additional <- c("scc_arg", "settlement_factor_arg", "exp_share_damages_paid_arg")
-    merge_by_cols <- c(merge_by_cols, lrisk_additional)
-  }
 
   crispy_output <- results_list$company_technology_npv %>%
     dplyr::inner_join(
       results_list$company_pd_changes_overall,
       by = merge_by_cols
     )
+
+
+
+
+
 
   if (risk_type == "lrisk") {
     select_cols <- c(merge_by_cols, "ald_business_unit", "company_is_litigated", "settlement")
@@ -218,10 +214,19 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars, risk_type) 
         settlement_factor = .data$settlement_factor_arg,
         exp_share_damages_paid = .data$exp_share_damages_paid_arg
       )
+  } else{
+    crispy_output <- crispy_output %>%
+      dplyr::rename(
+        carbon_price_model=.data$carbon_price_model_arg,
+        market_passthrough=.data$market_passthrough_arg,
+        financial_stimulus=.data$financial_stimulus_arg
+      )
   }
 
+
+
+
   crispy_output <- crispy_output %>%
-    dplyr::mutate(roll_up_type = "equity_ownership") %>%
     dplyr::rename(
       ald_sector = .data$ald_sector,
       ald_business_unit = .data$ald_business_unit,
@@ -236,9 +241,8 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars, risk_type) 
       net_present_value_shock = .data$total_disc_npv_ls,
       pd_baseline = .data$PD_baseline,
       pd_shock = .data$PD_late_sudden
-    )
-
-  crispy_output <- crispy_output %>%
+    ) %>%
+    dplyr::mutate(roll_up_type = "equity_ownership") %>%
     dplyr::mutate(
       net_present_value_difference = .data$net_present_value_shock - .data$net_present_value_baseline,
       net_present_value_roc = .data$net_present_value_shock/.data$net_present_value_baseline -1,
@@ -252,8 +256,9 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars, risk_type) 
         .data$roll_up_type, .data$scenario_geography,
         .data$baseline_scenario, .data$shock_scenario, .data$lgd,
         .data$risk_free_rate, .data$discount_rate, .data$dividend_rate,
-        .data$growth_rate, .data$scc, .data$settlement_factor, .data$exp_share_damages_paid,
-        .data$shock_year, .data$net_present_value_baseline,
+        .data$growth_rate, .data$scc, .data$settlement_factor,
+        .data$exp_share_damages_paid, .data$shock_year,
+        .data$net_present_value_baseline,
         .data$net_present_value_shock, .data$net_present_value_difference,
         .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference,
         .data$company_is_litigated, .data$settlement
@@ -265,6 +270,8 @@ wrangle_results <- function(results_list, sensitivity_analysis_vars, risk_type) 
         .data$roll_up_type, .data$scenario_geography,
         .data$baseline_scenario, .data$shock_scenario, .data$lgd,
         .data$risk_free_rate, .data$discount_rate, .data$dividend_rate,
+        .data$carbon_price_model, .data$market_passthrough,
+        .data$financial_stimulus,
         .data$growth_rate, .data$shock_year, .data$net_present_value_baseline,
         .data$net_present_value_shock, .data$net_present_value_difference, .data$net_present_value_roc,
         .data$term, .data$pd_baseline, .data$pd_shock, .data$pd_difference
