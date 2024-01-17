aa515a51eff01f0d4a09efb49050d8eba739ad54
bertrand <bertrand.gallice.pro@gmail.com>
2024-01-17 09:43:41 +0100

    wip

--- R/stress_test_arguments_numeric.R
+++ R/stress_test_arguments_numeric.R
@@ -1,5 +1,5 @@
 #' A dataset describing the arguments of [run_trisk()] and [run_lrisk()]
 #'
 #' @examples
-#' stress_test_arguments
-"stress_test_arguments"
+#' stress_test_arguments_numeric
+"stress_test_arguments_numeric"
